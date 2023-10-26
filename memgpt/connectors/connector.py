b2405e3983a73bf3a27d3e144181fa5096464368
Sarah Wooders <sarahwooders@gmail.com>
2023-10-26 12:02:29 -0700

    add db connector

--- memgpt/connectors/connector.py
+++ memgpt/connectors/connector.py
@@ -9,7 +9,6 @@ memgpt load <data-connector-type> --name <dataset-name> [ADDITIONAL ARGS]
 """
 
 from llama_index import download_loader
-from llama_index.embeddings import OpenAIEmbedding
 from typing import List
 import os
 import typer
@@ -63,4 +62,34 @@ def load_webpage(
     index = index_docs(docs)
     # save connector information into .memgpt metadata file
     save_index(index, name)
+
+
+@app.command("database")
+def load_database(
+    name: str = typer.Option(help="Name of dataset to load."),
+    scheme: str = typer.Option(help="Database scheme."),
+    host: str = typer.Option(help="Database host."),
+    port: int = typer.Option(help="Database port."),
+    user: str = typer.Option(help="Database user."),
+    password: str = typer.Option(help="Database password."),
+    dbname: str = typer.Option(help="Database name."),
+    query: str = typer.Option(None, help="Database query."),
+):
+    from llama_index.readers.database import DatabaseReader
+    
+    db = DatabaseReader(
+        scheme="postgresql",  # Database Scheme
+        host="localhost",  # Database Host
+        port="5432",  # Database Port
+        user="postgres",  # Database User
+        password="FakeExamplePassword",  # Database Password
+        dbname="postgres",  # Database Name
+    )
+
+    # load data
+    docs = db.load_data(query=query)
+    
+    index = index_docs(docs)
+    save_index(index, name)
+
 
