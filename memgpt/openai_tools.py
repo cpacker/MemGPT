2cf1a44c53739cb612d87fa4c29088e1b4c156c1
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-28 09:59:19 +0100

    Update openai_tools.py
    Updated to use the 'delay'

--- memgpt/openai_tools.py
+++ memgpt/openai_tools.py
@@ -47,7 +47,7 @@ def retry_with_exponential_backoff(
                 delay *= exponential_base * (1 + jitter * random.random())
 
                 # Sleep for the delay
-                time.sleep(62)
+                time.sleep(delay)
 
             # Raise exceptions for any errors not specified
             except Exception as e:
