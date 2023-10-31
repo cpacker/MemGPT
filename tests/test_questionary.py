e377dbf277e2ed8720f7f24e13b23a303d4e61e1
cpacker <packercharles@gmail.com>
2023-10-31 16:06:49 -0700

    added prints

--- tests/test_questionary.py
+++ tests/test_questionary.py
@@ -15,21 +15,26 @@ def test_cli_sequence():
 
     # Send 'Y' followed by newline
     child.sendline("Y")
+    print(child.before.decode())  # after child.sendline or child.expect
 
     # Expect a prompt or some output to know when to send the next command
     # child.expect("SomeExpectedOutputOrPromptRegex")
 
     # Send the '/save' command
     child.sendline("/save")
+    print(child.before.decode())  # after child.sendline or child.expect
 
     # Optionally, you can add more expects and sends here, depending on the interactions
     # child.expect("ExpectedOutputAfterSaveRegex")
 
     # Additional asserts can go here based on the outputs/behavior
     # Finally, make sure to gracefully exit the application if needed
-    child.sendline("exit")  # Replace with your CLI's exit command
+    child.sendline("/exit")  # Replace with your CLI's exit command
+    print(child.before.decode())  # after child.sendline or child.expect
 
     child.close()
+    assert child.isalive() is False, "CLI should have terminated."
+    assert child.exitstatus == 0, "CLI did not exit cleanly."
 
 
 if __name__ == "__main__":
