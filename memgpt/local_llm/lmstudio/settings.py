7434e0411f890334e998212bb592a452440ddddb
cpacker <packercharles@gmail.com>
2023-10-30 22:53:28 -0700

    8k not 3k

--- memgpt/local_llm/lmstudio/settings.py
+++ memgpt/local_llm/lmstudio/settings.py
@@ -9,10 +9,9 @@ SIMPLE = {
         # '\n#',
         # '\n\n\n',
     ],
-    # "max_tokens": 8000,
     # This controls the maximum number of tokens that the model can generate
     # Cap this at the model context length (assuming 8k for Mistral 7B)
-    "max_tokens": 3000,
+    "max_tokens": 8000,
     # This controls how LM studio handles context overflow
     # In MemGPT we handle this ourselves, so this should be commented out
     # "lmstudio": {"context_overflow_policy": 2},
