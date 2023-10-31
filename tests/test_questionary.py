8cdea1f0d55a1769b1979eafa8d7bc7d0d06d657
cpacker <packercharles@gmail.com>
2023-10-31 16:42:07 -0700

    updated test to match legacy flow

--- tests/test_questionary.py
+++ tests/test_questionary.py
@@ -14,27 +14,58 @@ def test_cli_sequence():
     # Start the CLI process
     child = pexpect.spawn("memgpt --first")
 
+    # Expect a prompt or some output to know when to send the next command
+    child.expect("Continue with legacy CLI?")
+
     # Send 'Y' followed by newline
     child.sendline("Y")
-    time.sleep(1)  # Wait for a short while to let output be captured
-    print(child.before.decode() if child.before else "No output captured")
+    # time.sleep(1)  # Wait for a short while to let output be captured
+    # print(child.before.decode() if child.before else "No output captured")
 
-    # Expect a prompt or some output to know when to send the next command
-    # child.expect("SomeExpectedOutputOrPromptRegex")
+    # Since .memgpt is empty, should jump immediately to "Which model?"
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Which model would you like to use?")
+    child.sendline()
+
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Which persona would you like MemGPT to use?")
+    child.sendline()
+
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Which user would you like to use?")
+    child.sendline()
+
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Would you like to preload anything into MemGPT's archival memory?")
+    child.sendline("N")
+
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Enter your message")
+    child.sendline("/save")
+
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Saved config file")
+    child.sendline("/load")
+
+    time.sleep(0.5)  # Wait for a short while to let output be captured
+    child.expect("Loaded persistence manager")
+    child.sendline("/exit")
+
+    child.expect("Finished.")
 
     # Send the '/save' command
-    child.sendline("/save")
-    time.sleep(1)  # Wait for a short while to let output be captured
-    print(child.before.decode() if child.before else "No output captured")
+    # child.sendline("/save")
+    # time.sleep(1)  # Wait for a short while to let output be captured
+    # print(child.before.decode() if child.before else "No output captured")
 
     # Optionally, you can add more expects and sends here, depending on the interactions
     # child.expect("ExpectedOutputAfterSaveRegex")
 
     # Additional asserts can go here based on the outputs/behavior
     # Finally, make sure to gracefully exit the application if needed
-    child.sendline("/exit")  # Replace with your CLI's exit command
-    time.sleep(1)  # Wait for a short while to let output be captured
-    print(child.before.decode() if child.before else "No output captured")
+    # child.sendline("/exit")  # Replace with your CLI's exit command
+    # time.sleep(1)  # Wait for a short while to let output be captured
+    # print(child.before.decode() if child.before else "No output captured")
 
     child.close()
     assert child.isalive() is False, "CLI should have terminated."
