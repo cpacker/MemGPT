8787936820c8b7b7b5f95ded5571db7bf987d9d4
Sarah Wooders <sarahwooders@gmail.com>
2023-11-02 14:33:24 -0700

    working postgres integration

--- memgpt/memory.py
+++ memgpt/memory.py
@@ -30,15 +30,7 @@ from llama_index.retrievers import VectorIndexRetriever
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.indices.postprocessor import SimilarityPostprocessor
 
-<<<<<<< HEAD
 from memgpt.embeddings import embedding_model
-from memgpt.config import MemGPTConfig
-
-from memgpt.embeddings import embedding_model
-=======
-from memgpt.embeddings import Index, embedding_model
-from memgpt.connectors.storage import StorageConnector, Passage
->>>>>>> b55cdad (add postgres connector)
 from memgpt.config import MemGPTConfig
 
 
