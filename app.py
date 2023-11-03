ac0712e5f29d7af9481b671e13ccaed14482f276
Doc <drrohinshadow>
2023-11-03 15:41:45 -0700

    Add Python application workflow and update MemGPT configuration

--- app.py
+++ app.py
@@ -15,18 +15,17 @@ import torch
 import os
 import openai
 
-
 openai.api_key = os.environ.get("openaikey")
 
 config_list = [
-    {"model": "gpt-4"},
+    {"model": "gpt-4", "temperature": 0.5, "top_p": 0.9, "presence_penalty": 0.0, "frequency_penalty": 0.0, "max_tokens": 1024},
 ]
 
 llm_config = {"config_list": config_list, "seed": 42}
 
 openai.api_key = os.environ.get("openaikey")
 interface = autogen_interface.AutoGenInterface()  # how MemGPT talks to AutoGen
-persistence_manager = InMemoryStateManagerWithFaiss()  # how MemGPT stores messages
+persistence_manager = InMemoryStateManager()  # how MemGPT stores messages
 agent_config = {
     "max_tokens": 1024,
     "temperature": 0.5,
