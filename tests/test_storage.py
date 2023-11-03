13f4bf960d0bb34c1a5ee9118e5642450ce34797
Sarah Wooders <sarahwooders@gmail.com>
2023-11-02 18:20:42 -0700

    add tests

---
+++ tests/test_storage.py
@@ -0,0 +1,33 @@
+from memgpt.connectors.storage import StorageConnector, PostgresStorageConnector, Passage
+from memgpt.embeddings import embedding_model
+from memgpt.config import MemGPTConfigS
+
+import argparse
+
+
+def test_postgres():
+
+    config = MemGPTConfig.load()
+    embed_model = embedding_model(config)
+
+    passage = ["This is a test passage", "This is another test passage", "Cinderella wept"]
+
+    db = PostgresStorageConnector(uri=config.archival_storage_uri, table_name="test2")
+
+    for passage in passage:
+        db.insert(Passage(text=passage, embedding=embed_model.get_text_embedding(passage)))
+
+    print(db.get_all())
+
+    query = "why was she crying"
+    query_vec = embed_model.get_text_embedding(query)
+    res = db.query(query_vec, top_k=2)
+
+    assert len(res) == 2, f"Expected 2 results, got {len(res)}"
+    assert "wept" in res[0].text, f"Expected 'wept' in results, but got {res[0].text}"
+
+    print("deleting")
+    db.delete()
+
+
+test_postgres()
