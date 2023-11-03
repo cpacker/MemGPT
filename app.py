aa97fbc2f543f48ca1465c83b8c3e16c6f2f0cf3
Doc <drrohinshadow>
2023-11-02 23:35:42 -0700

    testing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import autogen
 import memgpt.autogen.memgpt_agent as memgpt_autogen
-import memgpt.autogen.interface as autogen_interface 
+import memgpt.autogen.interface as autogen_interface
 import memgpt.agent as agent
 import memgpt.system as system
 import memgpt.utils as utils
@@ -11,16 +11,15 @@ import memgpt.personas.personas as personas
 import memgpt.humans.humans as humans
 from memgpt.persistence_manager import InMemoryStateManager, InMemoryStateManagerWithPreloadedArchivalMemory, InMemoryStateManagerWithFaiss
 import openai
-openai.api_key = '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'
-openai.api_key = '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'
-openai.api_key = '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'
-openai.api_key = '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'
-openai.api_key = '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'
+
+openai.api_key = "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"
+openai.api_key = "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"
+openai.api_key = "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"
+openai.api_key = "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"
+openai.api_key = "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"
 
 config_list = [
-    {
-        'model': 'gpt-4'
-    },
+    {"model": "gpt-4"},
 ]
 
 llm_config = {"config_list": config_list, "seed": 42}
@@ -30,11 +29,11 @@ user_proxy = autogen.UserProxyAgent(
     code_execution_config={"last_n_messages": 2, "work_dir": "groupchat"},
 )
 
-interface = autogen_interface.AutoGenInterface() # how MemGPT talks to AutoGen
+interface = autogen_interface.AutoGenInterface()  # how MemGPT talks to AutoGen
 persistence_manager = InMemoryStateManager()
-persona = "I\'m a 10x engineer at a FAANG tech company."
-human = "I\'m a team manager at a FAANG tech company."
-memgpt_agent = presets.use_preset(presets.DEFAULT_PRESET, 'gpt-4', persona, human, interface, persistence_manager)
+persona = "I'm a 10x engineer at a FAANG tech company."
+human = "I'm a team manager at a FAANG tech company."
+memgpt_agent = presets.use_preset(presets.DEFAULT_PRESET, "gpt-4", persona, human, interface, persistence_manager)
 
 # MemGPT coder
 coder = memgpt_autogen.MemGPTAgent(
