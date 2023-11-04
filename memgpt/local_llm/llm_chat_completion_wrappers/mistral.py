9d32e7a98a3f4402f6d1de064fb6a21b3e10c66a
Web 3 Wes <97464975+web3wes@users.noreply.github.com>
2023-11-04 04:28:57 -0400

    Update mistral.py

--- memgpt/local_llm/llm_chat_completion_wrappers/mistral.py
+++ memgpt/local_llm/llm_chat_completion_wrappers/mistral.py
@@ -12,11 +12,7 @@ B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
 class MistralChatCompletionWrapper(LLMChatCompletionWrapper):
     def chat_completion_to_prompt(self, messages, functions):
-        print(" VVVVVVV")
-        # print(messages)
         function_list = "\n".join(json.dumps(func, indent=4) for func in functions)
-        # system_prompt = ' '.join(msg['content'] for msg in messages if msg['role'] == 'system')
-        # user_prompt = ' '.join(msg['content'] for msg in messages if msg['role'] == 'user')
 
         # #if you don't do this the chat goes off the rails because has too many instructions
         # # Separate messages into system and user lists
@@ -35,35 +31,10 @@ class MistralChatCompletionWrapper(LLMChatCompletionWrapper):
         # And keep the last user message for user_prompt
         user_prompt = last_user_message
 
-        # print(f"{B_FUNC}{function_list.strip()}{E_FUNC}{B_INST} {B_SYS}{system_prompt.strip()}{E_SYS}{user_prompt.strip()} {E_INST}\n\n")
         print(type(user_prompt))
         return f"{B_FUNC}{function_list.strip()}{E_FUNC}{B_SYS}{system_prompt.strip()}{E_SYS}{B_INST}{user_prompt.strip()} {E_INST}\n\n"
 
-    # def output_to_chat_completion_response(self, raw_llm_output):
-    #     try:
-    #         raw_output_json = json.loads(raw_llm_output)
-
-    #         # print(raw_llm_output)
-    #         print("raw_output_json")
-    #         print(raw_output_json)
-    #         response_content = raw_output_json
-    #         response_json_start = response_content.find('{')
-    #         response_json_end = response_content.rfind('}') + 1
-    #         response_json = json.loads(response_content[response_json_start:response_json_end])
-    #         return {
-    #             'role': 'assistant',
-    #             # 'content': response_content,
-    #             'content': response_content[:response_json_start].strip(),
-    #             'function': response_json.get('function'),
-    #             'args': response_json.get('arguments', {})
-    #         }
-    #     except json.JSONDecodeError:
-    #         return {
-    #             'role': 'assistant',
-    #             'content': raw_llm_output,
-    #             'function': None,
-    #             'args': {}
-    #         }
+    
     def output_to_chat_completion_response(self, raw_llm_output):
         try:
             # Parse raw_llm_output into a dictionary
