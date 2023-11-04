ad25bbdcab8feb6fb4653dacd38c0290b7ce2d73
Charles Packer <packercharles@gmail.com>
2023-11-04 12:02:44 -0700

    Add grammar-based sampling (for webui, llamacpp, and koboldcpp) (#293)
    * add llamacpp server support
    
    * use gbnf loader
    
    * cleanup and warning about grammar when not using llama.cpp
    
    * added memgpt-specific grammar file
    
    * add grammar support to webui api calls
    
    * black
    
    * typo
    
    * add koboldcpp support
    
    * no more defaulting to webui, should error out instead
    
    * fix grammar
    
    * patch kobold (testing, now working) + cleanup log messages
    
    Co-Authored-By: Drake-AI <drake-ai@users.noreply.github.com>

--- memgpt/local_llm/utils.py
+++ memgpt/local_llm/utils.py
@@ -1,3 +1,6 @@
+import os
+
+
 class DotDict(dict):
     """Allow dot access on properties similar to OpenAI response object"""
 
@@ -13,3 +16,18 @@ class DotDict(dict):
 
     def __setstate__(self, state):
         vars(self).update(state)
+
+
+def load_grammar_file(grammar):
+    # Set grammar
+    grammar_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "grammars", f"{grammar}.gbnf")
+
+    # Check if the file exists
+    if not os.path.isfile(grammar_file):
+        # If the file doesn't exist, raise a FileNotFoundError
+        raise FileNotFoundError(f"The grammar file {grammar_file} does not exist.")
+
+    with open(grammar_file, "r") as file:
+        grammar_str = file.read()
+
+    return grammar_str
