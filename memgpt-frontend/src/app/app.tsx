e4e9264f6b81d024d8506f1d5432d6ddcfdf083b
robingotz <tug29225@temple.edu>
2023-11-06 21:44:41 -0500

    feat: display messages based on role and show inner thoughts and connection status

--- memgpt-frontend/src/app/app.tsx
+++ memgpt-frontend/src/app/app.tsx
@@ -4,37 +4,20 @@ import Header from './header';
 import MessageContainer from './messages/message-container';
 import { useCallback, useEffect, useState } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { useMessages } from './use-messages';
+import StatusIndicator from './status-indicator';
+
+
 export function App() {
-  const [socketUrl, setSocketUrl] = useState('ws://localhost:8000/api/ws');
-  const [messageHistory, setMessageHistory] = useState<string[]>([]);
-
-  const { sendMessage, lastMessage, readyState } = useWebSocket<string>(socketUrl);
-
-  useEffect(() => {
-    if (lastMessage !== null) {
-      setMessageHistory((prev) => [...prev, lastMessage.data]);
-    }
-  }, [lastMessage, setMessageHistory]);
-
-  const handleClickSendMessage = useCallback((message: string) => {
-    console.log('calling');
-    setMessageHistory((h) => [...h, message]);
-    sendMessage(message);
-  }, []);
-
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState];
+  const { readyState, messageHistory, sendMessage } = useMessages();
 
   return (
-    <div className="mx-auto max-w-screen-lg p-4">
-      <Header />
-      <MessageContainer messages={messageHistory} />
-      <UserInput onSend={handleClickSendMessage} />
+    <div className='mx-auto max-w-screen-lg p-4'>
+      <Header>
+        <StatusIndicator readyState={readyState}/>
+      </Header>
+        <MessageContainer messages={messageHistory} />
+      <UserInput onSend={sendMessage} />
       <Footer />
     </div>
   );
--- memgpt-frontend/src/app/messages/message-container.tsx
+++ memgpt-frontend/src/app/messages/message-container.tsx
@@ -2,8 +2,21 @@ import React, { useEffect, useRef } from 'react';
 import MemgptMessage from './message/memgpt-message';
 import UserMessage from './message/user-message';
 import { cnMuted } from '@memgpt/components/typography';
+import { Message } from '../use-messages';
 
-const MessageContainer = ({ messages }: { messages: string[] }) => {
+const getMessage = ({role, content, function_call}: Message, key: number) => {
+  if (role === 'user') {
+    return <UserMessage key={key} date={new Date()} message={content} />
+  }
+  if (role === 'assistant') {
+    return <div key={key}>
+      <p className={cnMuted('mb-2 w-fit text-xs p-2 rounded border')}>{content}</p>
+      <MemgptMessage date={new Date()} message={function_call?.arguments['message']} />
+    </div>
+  }
+  return undefined
+}
+const MessageContainer = ({ messages }: { messages: Message[] }) => {
   const messageBox = useRef<HTMLDivElement>(null);
   useEffect(() => messageBox.current?.scrollIntoView(false), [messages]);
   const messageLayout =
@@ -11,13 +24,7 @@ const MessageContainer = ({ messages }: { messages: string[] }) => {
       <p className={cnMuted('text-center p-20')}>Send a message to start your conversation...</p>
     ) : (
       <div className="flex flex-col flex-1 px-4 py-6 space-y-4" ref={messageBox}>
-        {messages.map((m, i) =>
-          i % 2 == 1 ? (
-            <MemgptMessage key={i} date={new Date()} message={m} />
-          ) : (
-            <UserMessage key={i} date={new Date()} message={m} />
-          )
-        )}
+        {messages.map((message, i) => getMessage(message, i)).filter(e => !!e)}
       </div>
     );
 
