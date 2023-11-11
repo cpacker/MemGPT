b2739d013e2e17ae8dcfe29b2d1f1079de163543
cpacker <packercharles@gmail.com>
2023-11-11 12:57:24 -0800

    refactored ws interface to be blocking since base agent is now sync

--- memgpt/server/websocket_interface.py
+++ memgpt/server/websocket_interface.py
@@ -3,42 +3,23 @@ import asyncio
 from ..interface import AgentInterface
 
 
-class WebSocketInterface(AgentInterface):
-    """Interface for interacting with a MemGPT agent over a WebSocket"""
-
-    def __init__(self):
-        pass
-
-    async def user_message(self, msg):
-        """MemGPT receives a user message"""
-        pass
-
-    async def internal_monologue(self, msg):
-        """MemGPT generates some internal monologue"""
-        pass
-
-    async def assistant_message(self, msg):
-        """MemGPT uses send_message"""
-        pass
-
-    async def function_message(self, msg):
-        """MemGPT calls a function"""
-        pass
-
-
-class WebSocketInterface(AgentInterface):
+class BaseWebSocketInterface(AgentInterface):
     """Interface for interacting with a MemGPT agent over a WebSocket"""
 
     def __init__(self):
         self.clients = set()
 
-    async def register_client(self, websocket):
+    def register_client(self, websocket):
         """Register a new client connection"""
         self.clients.add(websocket)
 
-    async def unregister_client(self, websocket):
+    def unregister_client(self, websocket):
         """Unregister a client connection"""
         self.clients.remove(websocket)
+
+
+class AsyncWebSocketInterface(BaseWebSocketInterface):
+    """WebSocket calls are async"""
 
     async def user_message(self, msg):
         """Handle reception of a user message"""
@@ -62,3 +43,33 @@ class WebSocketInterface(AgentInterface):
         # Send the function call message to all clients
         if self.clients:
             await asyncio.gather(*[client.send(f"Function call: {msg}") for client in self.clients])
+
+
+class BlockingWebSocketInterface(BaseWebSocketInterface):
+    """No async signatures, calls are blocking instead"""
+
+    def user_message(self, msg):
+        """Handle reception of a user message"""
+        # Logic to process the user message and possibly trigger agent's response
+        pass
+
+    def internal_monologue(self, msg):
+        """Handle the agent's internal monologue"""
+        if self.clients:
+            loop = asyncio.get_event_loop()
+            tasks = [loop.create_task(client.send(f"Internal monologue: {msg}")) for client in self.clients]
+            loop.run_until_complete(asyncio.gather(*tasks))
+
+    def assistant_message(self, msg):
+        """Handle the agent sending a message"""
+        if self.clients:
+            loop = asyncio.get_event_loop()
+            tasks = [loop.create_task(client.send(f"Assistant message: {msg}")) for client in self.clients]
+            loop.run_until_complete(asyncio.gather(*tasks))
+
+    def function_message(self, msg):
+        """Handle the agent calling a function"""
+        if self.clients:
+            loop = asyncio.get_event_loop()
+            tasks = [loop.create_task(client.send(f"Function call: {msg}")) for client in self.clients]
+            loop.run_until_complete(asyncio.gather(*tasks))
