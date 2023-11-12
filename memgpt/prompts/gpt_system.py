0faa80bc3ee5c59b010533f21f1f04c352b6a291
cpacker <packercharles@gmail.com>
2023-11-12 14:12:58 -0800

    allow looking for system prompts in ~/.memgpt/system_prompts

--- memgpt/prompts/gpt_system.py
+++ memgpt/prompts/gpt_system.py
@@ -1,12 +1,21 @@
 import os
 
+from memgpt.constants import MEMGPT_DIR
+
 
 def get_system_text(key):
     filename = f"{key}.txt"
     file_path = os.path.join(os.path.dirname(__file__), "system", filename)
 
+    # first look in prompts/system/*.txt
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
             return file.read().strip()
     else:
-        raise FileNotFoundError(f"No file found for key {key}, path={file_path}")
+        # try looking in ~/.memgpt/system_prompts/*.txt
+        file_path = os.path.join(MEMGPT_DIR, "system_prompts", filename)
+        if os.path.exists(file_path):
+            with open(file_path, "r") as file:
+                return file.read().strip()
+        else:
+            raise FileNotFoundError(f"No file found for key {key}, path={file_path}")
