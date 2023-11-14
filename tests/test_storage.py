fa3129393b003e957beb70f621d49f7fb99d54b7
Admin <prasantdixit987@gmail.com>
2023-11-14 18:22:20 +0530

    added test storage for lancedb

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -8,7 +8,7 @@ subprocess.check_call(
 import pgvector  # Try to import again after installing
 
 from memgpt.connectors.storage import StorageConnector, Passage
-from memgpt.connectors.db import PostgresStorageConnector
+from memgpt.connectors.db import PostgresStorageConnector, LanceDBConnector
 from memgpt.embeddings import embedding_model
 from memgpt.config import MemGPTConfig, AgentConfig
 
@@ -21,7 +21,7 @@ def test_postgres_openai():
         return  # soft pass
 
     # os.environ["MEMGPT_CONFIG_PATH"] = "./config"
-    config = MemGPTConfig(archival_storage_type="postgres", archival_storage_uri=os.getenv("PGVECTOR_TEST_DB_URL"))
+    config = MemGPTConfig(archival_storage_type="lancedb", archival_storage_uri=os.getenv("PGVECTOR_TEST_DB_URL"))
     print(config.config_path)
     assert config.archival_storage_uri is not None
     config.archival_storage_uri = config.archival_storage_uri.replace(
@@ -52,6 +52,34 @@ def test_postgres_openai():
     # print("deleting...")
     # db.delete()
     # print("...finished")
+
+def test_lancedb_openai():
+    assert os.getenv("LANCEDB_TEST_URL") is not None
+    if os.getenv("OPENAI_API_KEY") is None:
+        return  # soft pass
+
+    config = MemGPTConfig(archival_storage_type="lancedb", archival_storage_uri=os.getenv("LANCEDB_TEST_URL"))
+    print(config.config_path)
+    assert config.archival_storage_uri is not None
+    print(config)
+
+    embed_model = embedding_model()
+
+    passage = ["This is a test passage", "This is another test passage", "Cinderella wept"]
+
+    db = LanceDBConnector(name="test-openai")
+
+    for passage in passage:
+        db.insert(Passage(text=passage, embedding=embed_model.get_text_embedding(passage)))
+
+    print(db.get_all())
+
+    query = "why was she crying"
+    query_vec = embed_model.get_text_embedding(query)
+    res = db.query(None, query_vec, top_k=2)
+
+    assert len(res) == 2, f"Expected 2 results, got {len(res)}"
+    assert "wept" in res[0].text, f"Expected 'wept' in results, but got {res[0].text}"
 
 
 def test_postgres_local():
@@ -95,5 +123,32 @@ def test_postgres_local():
     # db.delete()
     # print("...finished")
 
+def test_lancedb_local():
+    assert os.getenv("LANCEDB_TEST_URL") is not None
+    
+    config = MemGPTConfig(
+        archival_storage_type="lancedb",
+        archival_storage_uri=os.getenv("LANCEDB_TEST_URL"),
+        embedding_model="local",
+        embedding_dim=384,  # use HF model
+    )
+    print(config.config_path)
+    assert config.archival_storage_uri is not None
+    
+    embed_model = embedding_model()
 
-# test_postgres()
+    passage = ["This is a test passage", "This is another test passage", "Cinderella wept"]
+
+    db = LanceDBConnector(name="test-local")
+
+    for passage in passage:
+        db.insert(Passage(text=passage, embedding=embed_model.get_text_embedding(passage)))
+
+    print(db.get_all())
+
+    query = "why was she crying"
+    query_vec = embed_model.get_text_embedding(query)
+    res = db.query(None, query_vec, top_k=2)
+
+    assert len(res) == 2, f"Expected 2 results, got {len(res)}"
+    assert "wept" in res[0].text, f"Expected 'wept' in results, but got {res[0].text}"
