c2af7dbf7887bdf52ca4802a9ce94227ea0aae18
Charles Packer <packercharles@gmail.com>
2023-11-16 22:50:00 -0800

    updated websocket protocol and server (#473)

--- memgpt/server/utils.py
+++ memgpt/server/utils.py
@@ -1,6 +1,9 @@
 def condition_to_stop_receiving(response):
     """Determines when to stop listening to the server"""
-    return response.get("type") == "agent_response_end"
+    if response.get("type") in ["agent_response_end", "agent_response_error", "command_response", "server_error"]:
+        return True
+    else:
+        return False
 
 
 def print_server_response(response):
