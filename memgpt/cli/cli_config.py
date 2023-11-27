2d6b0d47aa81052ff4caf6c4b573e2492663cceb
Sarah Wooders <sarahwooders@gmail.com>
2023-11-27 15:33:37 -0800

    fix configuration

--- memgpt/cli/cli_config.py
+++ memgpt/cli/cli_config.py
@@ -81,6 +81,8 @@ def configure_llm_endpoint(config: MemGPTConfig):
             if model_endpoint_type in DEFAULT_ENDPOINTS:
                 default_model_endpoint = DEFAULT_ENDPOINTS[model_endpoint_type]
                 model_endpoint = questionary.text("Enter default endpoint:", default=default_model_endpoint).ask()
+            elif config.model_endpoint:
+                model_endpoint = questionary.text("Enter default endpoint:", default=config.model_endpoint).ask()
             else:
                 # default_model_endpoint = None
                 model_endpoint = None
@@ -174,7 +176,7 @@ def configure_embedding_endpoint(config: MemGPTConfig):
 
     default_embedding_endpoint_type = config.embedding_endpoint_type
 
-    embedding_endpoint_type, embedding_endpoint, embedding_dim, embedding_model = None, None, None, embedding_model
+    embedding_endpoint_type, embedding_endpoint, embedding_dim, embedding_model = None, None, None, None
     embedding_provider = questionary.select(
         "Select embedding provider:", choices=["openai", "azure", "hugging-face", "local"], default=default_embedding_endpoint_type
     ).ask()
@@ -199,13 +201,15 @@ def configure_embedding_endpoint(config: MemGPTConfig):
             embedding_endpoint = None
 
         # get model type
+        default_embedding_model = config.embedding_model if config.embedding_model else "BAAI/bge-large-en-v1.5"
         embedding_model = questionary.text(
             "Enter HuggingFace model tag (e.g. BAAI/bge-large-en-v1.5):",
-            default=default_model,
+            default=default_embedding_model,
         ).ask()
 
         # get model dimentions
-        embedding_dim = questionary.text("Enter embedding model dimentions (e.g. 1024):", default=default_embedding_dim).ask()
+        default_embedding_dim = config.embedding_dim if config.embedding_dim else "1024"
+        embedding_dim = questionary.text("Enter embedding model dimentions (e.g. 1024):", default=str(default_embedding_dim)).ask()
         try:
             embedding_dim = int(embedding_dim)
         except Exception as e:
@@ -215,7 +219,7 @@ def configure_embedding_endpoint(config: MemGPTConfig):
         embedding_endpoint = None
         embedding_dim = 384
 
-    return embedding_endpoint_type, embedding_endpoint, embedding_dim
+    return embedding_endpoint_type, embedding_endpoint, embedding_dim, embedding_model
 
 
 def configure_cli(config: MemGPTConfig):
@@ -276,7 +280,7 @@ def configure():
     config = MemGPTConfig.load()
     model_endpoint_type, model_endpoint = configure_llm_endpoint(config)
     model, model_wrapper, context_window = configure_model(config, model_endpoint_type)
-    embedding_endpoint_type, embedding_endpoint, embedding_dim = configure_embedding_endpoint(config)
+    embedding_endpoint_type, embedding_endpoint, embedding_dim, embedding_model = configure_embedding_endpoint(config)
     default_preset, default_persona, default_human, default_agent = configure_cli(config)
     archival_storage_type, archival_storage_uri = configure_archival_storage(config)
 
@@ -309,6 +313,7 @@ def configure():
         embedding_endpoint_type=embedding_endpoint_type,
         embedding_endpoint=embedding_endpoint,
         embedding_dim=embedding_dim,
+        embedding_model=embedding_model,
         # cli configs
         preset=default_preset,
         persona=default_persona,
