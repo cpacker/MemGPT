42eb680b948aed210513b4b227e3bf99973ff41a
Charles Packer <packercharles@gmail.com>
2023-11-27 16:41:42 -0800

    Clean memory error messages (#523)
    * Raise a custom keyerror instead of basic keyerror to clarify issue to LLM processor
    
    * remove self value from error message passed to LLM processor
    
    * simplify error message propogated to llm processor

--- memgpt/agent.py
+++ memgpt/agent.py
@@ -515,10 +515,14 @@ class Agent(object):
             try:
                 function_args["self"] = self  # need to attach self to arg since it's dynamically linked
                 function_response_string = function_to_call(**function_args)
+                function_args.pop("self", None)
                 function_response = package_function_response(True, function_response_string)
                 function_failed = False
             except Exception as e:
-                error_msg = f"Error calling function {function_name} with args {function_args}: {str(e)}"
+                function_args.pop("self", None)
+                # error_msg = f"Error calling function {function_name} with args {function_args}: {str(e)}"
+                # Less detailed - don't provide full args, idea is that it should be in recent context so no need (just adds noise)
+                error_msg = f"Error calling function {function_name}: {str(e)}"
                 error_msg_user = f"{error_msg}\n{traceback.format_exc()}"
                 printd(error_msg_user)
                 function_response = package_function_response(False, error_msg)
