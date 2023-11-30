8a4382ff8780cf8889d5d2f134d34213d42003d0
Sarah Wooders <sarahwooders@gmail.com>
2023-11-30 12:12:38 -0800

    message

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from memgpt.main import app
 import typer
 
-typer.secho("Command `python main.py` no longer supported. Please run `memgpt run`.", fg=typer.colors.YELLOW)
+typer.secho(
+    "Command `python main.py` no longer supported. Please run `memgpt run`. See https://memgpt.readthedocs.io/en/latest/quickstart/.",
+    fg=typer.colors.YELLOW,
+)
