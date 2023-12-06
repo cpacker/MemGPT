99ee64124dd57b9708058772f0677c8a339b3ee0
Charles Packer <packercharles@gmail.com>
2023-12-05 23:01:37 -0800

    fix runtime error (#586)

--- memgpt/memory.py
+++ memgpt/memory.py
@@ -8,6 +8,8 @@ from memgpt.utils import get_local_time, printd, count_tokens, validate_date_for
 from memgpt.prompts.gpt_summarize import SYSTEM as SUMMARY_PROMPT_SYSTEM
 from memgpt.openai_tools import create
 from memgpt.data_types import Message, Passage
+from memgpt.config import MemGPTConfig
+from memgpt.embeddings import embedding_model
 from llama_index import Document
 from llama_index.node_parser import SimpleNodeParser
 from llama_index.node_parser import SimpleNodeParser
