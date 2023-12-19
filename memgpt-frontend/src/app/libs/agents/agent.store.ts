46854e186f7ee97c7ce71cdb9499976875e5060a
robingotz <tug29225@temple.edu>
2023-12-19 21:15:27 +0100

    feat: new iteration of chatui - beware it is still buggy
    added some error handling, but I believe this still needs a lot of improvements.
    added timestamps for when messages are sent.
    when changing to a new agent the agent initiates the conversation.
    persisting messages for now. Storing them in localstorage so users can see their
    history and don't lose it on reload. replacing this with intelligent fetching asap.

--- memgpt-frontend/src/app/libs/agents/agent.store.ts
+++ memgpt-frontend/src/app/libs/agents/agent.store.ts
@@ -1,15 +1,36 @@
 import { create } from 'zustand';
 import { Agent } from './agent';
-import { combine } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
-const useAgentStore = create(combine({
-  currentAgent: null as Agent | null,
-}, (set) => ({
-  actions: {
-    setAgent: (agent: Agent) => set({ currentAgent: agent }),
-    removeAgent: () => set({ currentAgent: null }),
-  },
-})));
+const useAgentStore = create(
+  persist<{
+    currentAgent: Agent | null,
+    lastAgentInitMessage: { date: Date, agentId: string } | null
+    actions: {
+      setAgent: (agent: Agent) => void,
+      setLastAgentInitMessage: (messageInfo: { date: Date, agentId: string } | null) => void;
+      removeAgent: () => void
+    }
+  }>(
+    (set, get) => ({
+      currentAgent: null,
+      lastAgentInitMessage: null,
+      actions: {
+        setAgent: (agent: Agent) => set({ currentAgent: agent }),
+        setLastAgentInitMessage: (messageInfo: {
+          date: Date,
+          agentId: string
+        } | null) => set(prev => ({...prev, lastAgentInitMessage: messageInfo })),
+        removeAgent: () => set(prev => ({ ...prev, currentAgent: null })),
+      },
+    }),
+    {
+      name: 'agent-storage',
+      partialize: ({ actions, ...rest }: any) => rest,
+    },
+  ),
+);
 
 export const useCurrentAgent = () => useAgentStore(s => s.currentAgent);
+export const useLastAgentInitMessage = () => useAgentStore(s => s.lastAgentInitMessage);
 export const useAgentActions = () => useAgentStore(s => s.actions);
