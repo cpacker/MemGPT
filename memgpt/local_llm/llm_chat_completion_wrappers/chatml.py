b8f4e9805946c00d2573e1f3d744d5bc1d1cee98
Charles Packer <packercharles@gmail.com>
2023-12-24 23:46:00 -0800

    feat: added basic heartbeat override heuristics (#621)
    * added basic heartbeat override
    
    * tested and working on lmstudio (patched typo + patched new bug emerging in latest lmstudio build
    
    * added lmstudio patch to chatml wrapper
    
    * update the system messages to be informative about the source
    
    * updated string constants after some tuning

--- memgpt/local_llm/llm_chat_completion_wrappers/chatml.py
+++ memgpt/local_llm/llm_chat_completion_wrappers/chatml.py
@@ -255,10 +255,16 @@ class ChatMLInnerMonologueWrapper(LLMChatCompletionWrapper):
         except Exception as e:
             raise Exception(f"Failed to decode JSON from LLM output:\n{raw_llm_output} - error\n{str(e)}")
         try:
+            # NOTE: weird bug can happen where 'function' gets nested if the prefix in the prompt isn't abided by
+            if isinstance(function_json_output["function"], dict):
+                function_json_output = function_json_output["function"]
+            # regular unpacking
             function_name = function_json_output["function"]
             function_parameters = function_json_output["params"]
         except KeyError as e:
-            raise LLMJSONParsingError(f"Received valid JSON from LLM, but JSON was missing fields: {str(e)}")
+            raise LLMJSONParsingError(
+                f"Received valid JSON from LLM, but JSON was missing fields: {str(e)}. JSON result was:\n{function_json_output}"
+            )
 
         if self.clean_func_args:
             (
@@ -366,6 +372,7 @@ class ChatMLOuterInnerMonologueWrapper(ChatMLInnerMonologueWrapper):
                 and function_json_output["function"] is not None
                 and function_json_output["function"].strip().lower() != "none"
             ):
+                # TODO apply lm studio nested bug patch?
                 function_name = function_json_output["function"]
                 function_parameters = function_json_output["params"]
             else:
