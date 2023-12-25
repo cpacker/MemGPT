526bace972b0d012c4b98ea3ddd98157ff49463e
cpacker <packercharles@gmail.com>
2023-12-24 23:22:18 -0800

    set default temp to 0.8

--- memgpt/local_llm/settings/simple.py
+++ memgpt/local_llm/settings/simple.py
@@ -23,4 +23,6 @@ settings = {
         # NOTE: this requires the ability to patch the extra '}}' back into the prompt
         "  }\n}\n",
     ],
+    # most lm frontends default to 0.7-0.8 these days
+    "temperature": 0.8,
 }
