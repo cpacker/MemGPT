84c845a350a5b150df19fa7498fd7bc1fb456033
BabellDev <bellb77@gmail.com>
2023-12-26 18:49:04 -0500

    rolled back unnecessary changes to abstract interface; switched client to always use Queueing Interface

--- memgpt/client/client.py
+++ memgpt/client/client.py
@@ -1,9 +1,8 @@
-from typing import Union
+from typing import Dict, List, Union
 
 from memgpt.config import AgentConfig
-from memgpt.interface import AgentInterface
-from memgpt.interface import CLIInterface
 from memgpt.persistence_manager import PersistenceManager
+from memgpt.server.rest_api.interface import QueuingInterface
 from memgpt.server.server import SyncServer
 
 
@@ -11,63 +10,53 @@ class Client(object):
 
     def __init__(
         self,
-        interface: AgentInterface = CLIInterface(),
         auto_save: bool = False,
     ):
         self.user_id = "null"
         self.auto_save = auto_save
-        self.server = SyncServer(default_interface=interface)
+        self.interface = QueuingInterface()
+        self.server = SyncServer(default_interface=self.interface)
+
+    def list_agents(self):
+        self.interface.clear()
+        return self.server.list_agents(user_id=self.user_id)
 
     def create_agent(
         self,
-        agent_config: Union[dict, AgentConfig],
+        agent_config: Union[Dict, AgentConfig],
         persistence_manager: Union[PersistenceManager, None] = None,
     ) -> str:
+        self.interface.clear()
         return self.server.create_agent(
             user_id=self.user_id,
             agent_config=agent_config,
             persistence_manager=persistence_manager)
 
-    def user_message(self, agent_id: str, message: str):
+    def get_agent_config(self, agent_id: str) -> Dict:
+        self.interface.clear()
+        return self.server.get_agent_config(user_id=self.user_id, agent_id=agent_id)
+
+    def get_agent_memory(self, agent_id: str) -> Dict:
+        self.interface.clear()
+        return self.server.get_agent_memory(user_id=self.user_id, agent_id=agent_id)
+
+    def update_agent_core_memory(self, agent_id: str, new_memory_contents: Dict) -> Dict:
+        self.interface.clear()
+        return self.server.update_agent_core_memory(
+            user_id=self.user_id,
+            agent_id=agent_id,
+            new_memory_contents=new_memory_contents
+        )
+
+    def user_message(self, agent_id: str, message: str) -> List[Dict]:
+        self.interface.clear()
         self.server.user_message(user_id=self.user_id, agent_id=agent_id, message=message)
         if self.auto_save:
             self.save()
+        return self.interface.to_list()
+
+    def run_command(self, agent_id: str, command: str) -> Union[str, None]:
+        return self.server.run_command(user_id=self.user_id, agent_id=agent_id, command=command)
 
     def save(self):
         self.server.save_agents()
-
-
-if __name__ == "__main__":
-    from memgpt.persistence_manager import LocalStateManager
-    import memgpt.constants as constants
-    import memgpt.system as system
-    import memgpt.utils as utils
-    import memgpt.presets.presets as presets
-    from memgpt.config import AgentConfig
-    from memgpt.interface import CLIInterface as interface
-    from memgpt.constants import MEMGPT_DIR
-
-    # Create an AgentConfig option from the inputs
-    # agent_config = AgentConfig(
-    #     # name="agent_4",
-    #     name="tommy",
-    #     persona=constants.DEFAULT_PERSONA,
-    #     human=constants.DEFAULT_HUMAN,
-    #     preset="memgpt_chat",
-    #     model="gpt-4",
-    #     model_endpoint_type="openai",
-    #     model_endpoint="https://api.openai.com/v1",
-    #     context_window=8192,
-    # )
-    #
-    # client = Client(auto_save=True)
-    # name = client.create_agent(agent_config=agent_config)
-    # print(name)
-
-
-    #client.user_message(agent_id="agent_5", message="tell me my name please")
-    #client.user_message(agent_id="agent_5", message="I enjoy dry red wine")
-    #client.user_message(agent_id="agent_5", message="I have a dog named Tonic")
-    #client.save()
-    #client.user_message(agent_id="agent_5", message="What is my dog's name?")
-
