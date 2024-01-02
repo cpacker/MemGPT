5fd8b11abbe5d82746d98652d818c4ccc841590b
cpacker <packercharles@gmail.com>
2024-01-02 13:28:06 -0800

    clean up the error messages stemming from a badly configured endpoint (bad url, url is none, etc)

--- memgpt/cli/cli_load.py
+++ memgpt/cli/cli_load.py
@@ -88,26 +88,29 @@ def load_index(
     name: str = typer.Option(help="Name of dataset to load."), dir: str = typer.Option(help="Path to directory containing index.")
 ):
     """Load a LlamaIndex saved VectorIndex into MemGPT"""
-    # load index data
-    storage_context = StorageContext.from_defaults(persist_dir=dir)
-    loaded_index = load_index_from_storage(storage_context)
+    try:
+        # load index data
+        storage_context = StorageContext.from_defaults(persist_dir=dir)
+        loaded_index = load_index_from_storage(storage_context)
 
-    # hacky code to extract out passages/embeddings (thanks a lot, llama index)
-    embed_dict = loaded_index._vector_store._data.embedding_dict
-    node_dict = loaded_index._docstore.docs
+        # hacky code to extract out passages/embeddings (thanks a lot, llama index)
+        embed_dict = loaded_index._vector_store._data.embedding_dict
+        node_dict = loaded_index._docstore.docs
 
-    passages = []
-    for node_id, node in node_dict.items():
-        vector = embed_dict[node_id]
-        node.embedding = vector
-        passages.append(Passage(text=node.text, embedding=vector))
+        passages = []
+        for node_id, node in node_dict.items():
+            vector = embed_dict[node_id]
+            node.embedding = vector
+            passages.append(Passage(text=node.text, embedding=vector))
 
-    # create storage connector
-    storage = StorageConnector.get_archival_storage_connector(name=name)
+        # create storage connector
+        storage = StorageConnector.get_archival_storage_connector(name=name)
 
-    # add and save all passages
-    storage.insert_many(passages)
-    storage.save()
+        # add and save all passages
+        storage.insert_many(passages)
+        storage.save()
+    except ValueError as e:
+        typer.secho(f"Failed to load index from provided information.\n{e}", fg=typer.colors.RED)
 
 
 @app.command("directory")
@@ -117,22 +120,26 @@ def load_directory(
     input_files: List[str] = typer.Option(None, help="List of paths to files containing dataset."),
     recursive: bool = typer.Option(False, help="Recursively search for files in directory."),
 ):
-    from llama_index import SimpleDirectoryReader
+    try:
+        from llama_index import SimpleDirectoryReader
 
-    if recursive:
-        assert input_dir is not None, "Must provide input directory if recursive is True."
+        if recursive:
+            assert input_dir is not None, "Must provide input directory if recursive is True."
 
-    if input_dir is not None:
-        reader = SimpleDirectoryReader(
-            input_dir=input_dir,
-            recursive=recursive,
-        )
-    else:
-        reader = SimpleDirectoryReader(input_files=input_files)
+        if input_dir is not None:
+            reader = SimpleDirectoryReader(
+                input_dir=input_dir,
+                recursive=recursive,
+            )
+        else:
+            reader = SimpleDirectoryReader(input_files=input_files)
 
-    # load docs
-    docs = reader.load_data()
-    store_docs(name, docs)
+        # load docs
+        docs = reader.load_data()
+        store_docs(name, docs)
+
+    except ValueError as e:
+        typer.secho(f"Failed to load directory from provided information.\n{e}", fg=typer.colors.RED)
 
 
 @app.command("webpage")
@@ -140,10 +147,14 @@ def load_webpage(
     name: str = typer.Option(help="Name of dataset to load."),
     urls: List[str] = typer.Option(None, help="List of urls to load."),
 ):
-    from llama_index import SimpleWebPageReader
+    try:
+        from llama_index import SimpleWebPageReader
 
-    docs = SimpleWebPageReader(html_to_text=True).load_data(urls)
-    store_docs(name, docs)
+        docs = SimpleWebPageReader(html_to_text=True).load_data(urls)
+        store_docs(name, docs)
+
+    except ValueError as e:
+        typer.secho(f"Failed to load webpage from provided information.\n{e}", fg=typer.colors.RED)
 
 
 @app.command("database")
@@ -158,38 +169,41 @@ def load_database(
     password: str = typer.Option(None, help="Database password."),
     dbname: str = typer.Option(None, help="Database name."),
 ):
-    from llama_index.readers.database import DatabaseReader
+    try:
+        from llama_index.readers.database import DatabaseReader
 
-    print(dump_path, scheme)
+        print(dump_path, scheme)
 
-    if dump_path is not None:
-        # read from database dump file
-        from sqlalchemy import create_engine
+        if dump_path is not None:
+            # read from database dump file
+            from sqlalchemy import create_engine
 
-        engine = create_engine(f"sqlite:///{dump_path}")
+            engine = create_engine(f"sqlite:///{dump_path}")
 
-        db = DatabaseReader(engine=engine)
-    else:
-        assert dump_path is None, "Cannot provide both dump_path and database connection parameters."
-        assert scheme is not None, "Must provide database scheme."
-        assert host is not None, "Must provide database host."
-        assert port is not None, "Must provide database port."
-        assert user is not None, "Must provide database user."
-        assert password is not None, "Must provide database password."
-        assert dbname is not None, "Must provide database name."
+            db = DatabaseReader(engine=engine)
+        else:
+            assert dump_path is None, "Cannot provide both dump_path and database connection parameters."
+            assert scheme is not None, "Must provide database scheme."
+            assert host is not None, "Must provide database host."
+            assert port is not None, "Must provide database port."
+            assert user is not None, "Must provide database user."
+            assert password is not None, "Must provide database password."
+            assert dbname is not None, "Must provide database name."
 
-        db = DatabaseReader(
-            scheme=scheme,  # Database Scheme
-            host=host,  # Database Host
-            port=port,  # Database Port
-            user=user,  # Database User
-            password=password,  # Database Password
-            dbname=dbname,  # Database Name
-        )
+            db = DatabaseReader(
+                scheme=scheme,  # Database Scheme
+                host=host,  # Database Host
+                port=port,  # Database Port
+                user=user,  # Database User
+                password=password,  # Database Password
+                dbname=dbname,  # Database Name
+            )
 
-    # load data
-    docs = db.load_data(query=query)
-    store_docs(name, docs)
+        # load data
+        docs = db.load_data(query=query)
+        store_docs(name, docs)
+    except ValueError as e:
+        typer.secho(f"Failed to load database from provided information.\n{e}", fg=typer.colors.RED)
 
 
 @app.command("vector-database")
@@ -202,34 +216,38 @@ def load_vector_database(
 ):
     """Load pre-computed embeddings into MemGPT from a database."""
 
-    from sqlalchemy import create_engine, select, MetaData, Table, Inspector
-    from pgvector.sqlalchemy import Vector
+    try:
+        from sqlalchemy import create_engine, select, MetaData, Table, Inspector
+        from pgvector.sqlalchemy import Vector
 
-    # connect to db table
-    engine = create_engine(uri)
-    metadata = MetaData()
-    # Create an inspector to inspect the database
-    inspector = Inspector.from_engine(engine)
-    table_names = inspector.get_table_names()
-    assert table_name in table_names, f"Table {table_name} not found in database: tables that exist {table_names}."
+        # connect to db table
+        engine = create_engine(uri)
+        metadata = MetaData()
+        # Create an inspector to inspect the database
+        inspector = Inspector.from_engine(engine)
+        table_names = inspector.get_table_names()
+        assert table_name in table_names, f"Table {table_name} not found in database: tables that exist {table_names}."
 
-    table = Table(table_name, metadata, autoload_with=engine)
+        table = Table(table_name, metadata, autoload_with=engine)
 
-    config = MemGPTConfig.load()
+        config = MemGPTConfig.load()
 
-    # Prepare a select statement
-    select_statement = select(table.c[text_column], table.c[embedding_column].cast(Vector(config.embedding_dim)))
+        # Prepare a select statement
+        select_statement = select(table.c[text_column], table.c[embedding_column].cast(Vector(config.embedding_dim)))
 
-    # Execute the query and fetch the results
-    with engine.connect() as connection:
-        result = connection.execute(select_statement).fetchall()
+        # Execute the query and fetch the results
+        with engine.connect() as connection:
+            result = connection.execute(select_statement).fetchall()
 
-    # Convert to a list of tuples (text, embedding)
-    passages = []
-    for text, embedding in result:
-        passages.append(Passage(text=text, embedding=embedding))
-        assert config.embedding_dim == len(embedding), f"Expected embedding dimension {config.embedding_dim}, got {len(embedding)}"
+        # Convert to a list of tuples (text, embedding)
+        passages = []
+        for text, embedding in result:
+            passages.append(Passage(text=text, embedding=embedding))
+            assert config.embedding_dim == len(embedding), f"Expected embedding dimension {config.embedding_dim}, got {len(embedding)}"
 
-    # insert into storage
-    storage = StorageConnector.get_archival_storage_connector(name=name)
-    storage.insert_many(passages)
+        # insert into storage
+        storage = StorageConnector.get_archival_storage_connector(name=name)
+        storage.insert_many(passages)
+
+    except ValueError as e:
+        typer.secho(f"Failed to load vector database from provided information.\n{e}", fg=typer.colors.RED)
