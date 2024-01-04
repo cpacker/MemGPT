c00b40c7dc3378b531fe4bc380516f183e693181
Sarah Wooders <sarahwooders@gmail.com>
2024-01-04 15:08:34 -0800

    initial implementation of agent/user data types

--- memgpt/constants.py
+++ memgpt/constants.py
@@ -6,6 +6,8 @@ DEFAULT_MEMGPT_MODEL = "gpt-4"
 DEFAULT_PERSONA = "sam_pov"
 DEFAULT_HUMAN = "basic"
 
+from memgpt.constants import DEFAULT_HUMAN, DEFAULT_MEMGPT_MODEL, DEFAULT_PERSONA
+
 FIRST_MESSAGE_ATTEMPTS = 10
 
 INITIAL_BOOT_MESSAGE = "Boot sequence complete. Persona activated."
--- memgpt/data_types.py
+++ memgpt/data_types.py
@@ -4,6 +4,8 @@ from abc import abstractmethod
 from typing import Optional, List, Dict
 import numpy as np
 
+from memgpt.constants import DEFAULT_HUMAN, DEFAULT_MEMGPT_MODEL, DEFAULT_PERSONA, LLM_MAX_TOKENS
+from memgpt.presets.presets import DEFAULT_PRESET
 
 # Defining schema objects:
 # Note: user/agent can borrow from MemGPTConfig/AgentConfig classes
@@ -161,29 +163,17 @@ class User:
         default_human=DEFAULT_HUMAN,
         default_agent=None,
         # defaults: llm model
-        default_model=DEFAULT_MODEL,
-        default_model_endpoint_type=DEFAULT_MODEL_ENDPOINT_TYPE,
-        default_model_endpoint=DEFAULT_MODEL_ENDPOINT,
-        default_model_wrapper=DEFAULT_MODEL_WRAPPER,
-        default_context_window=DEFAULT_CONTEXT_WINDOW,
+        default_model=DEFAULT_MEMGPT_MODEL,
+        default_model_endpoint_type="openai",
+        default_model_endpoint="https://api.openai.com/v1",
+        default_model_wrapper=None,
+        default_context_window=None,
         # defaults: embeddings
         default_embedding_endpoint_type="memgpt_hosted",
         default_embedding_endpoint=None,
         default_embedding_model=None,
-        default_embedding_dim=DEFAULT_EMBEDDING_DIM,
-        default_embedding_chunk_size=DEFAULT_EMBEDDING_CHUNK_SIZE,
-        # storage: archival memory
-        default_archival_storage_type=DEFAULT_ARCHIVAL_STORAGE_TYPE,
-        default_archival_storage_path=DEFAULT_ARCHIVAL_STORAGE_PATH,
-        default_archival_storage_uri=DEFAULT_ARCHIVAL_STORAGE_URI,
-        # storage: recall memory
-        default_recall_storage_type=DEFAULT_RECALL_STORAGE_TYPE,
-        default_recall_storage_path=DEFAULT_RECALL_STORAGE_PATH,
-        default_recall_storage_uri=DEFAULT_RECALL_STORAGE_URI,
-        # storage: metadata
-        default_metadata_storage_type=DEFAULT_METADATA_STORAGE_TYPE,
-        default_metadata_storage_path=DEFAULT_METADATA_STORAGE_PATH,
-        default_metadata_storage_uri=DEFAULT_METADATA_STORAGE_URI,
+        default_embedding_dim=1536,
+        default_embedding_chunk_size=300,
         # azure information
         azure_key=None,
         azure_endpoint=None,
@@ -196,7 +186,42 @@ class User:
         memgpt_version=None,
         policies_accepted=False,
     ):
-        pass
+        self.default_preset = default_preset
+        self.default_persona = default_persona
+        self.default_human = default_human
+        self.default_agent = default_agent
+
+        # defaults: llm model
+        self.default_model = default_model
+        self.default_model_endpoint_type = default_model_endpoint_type
+        self.default_model_endpoint = default_model_endpoint
+        self.default_model_wrapper = default_model_wrapper
+        if default_context_window is None:
+            self.default_context_window = (
+                LLM_MAX_TOKENS[self.default_model] if self.default_model in LLM_MAX_TOKENS else LLM_MAX_TOKENS["DEFAULT"]
+            )
+        else:
+            self.default_context_window = default_context_window
+
+        # defaults: embeddings
+        self.default_embedding_endpoint_type = default_embedding_endpoint_type
+        self.default_embedding_endpoint = default_embedding_endpoint
+        self.default_embedding_model = default_embedding_model
+        self.default_embedding_dim = default_embedding_dim
+        self.default_embedding_chunk_size = default_embedding_chunk_size
+
+        # azure information
+        self.azure_key = azure_key
+        self.azure_endpoint = azure_endpoint
+        self.azure_version = azure_version
+        self.azure_deployment = azure_deployment
+
+        # openai information
+        self.openai_key = openai_key
+
+        # TODO: generate
+        self.anon_clientid = anon_clientid
+        self.policies_accepted = policies_accepted
 
 
 class AgentState(Record):
