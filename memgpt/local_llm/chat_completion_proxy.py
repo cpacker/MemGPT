ef5e6700281eba0fc7132eee7fe8d31b9212dd28
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-01-14 22:10:54 +0100

    Update chat_completion_proxy.py

--- memgpt/local_llm/chat_completion_proxy.py
+++ memgpt/local_llm/chat_completion_proxy.py
@@ -108,7 +108,7 @@ def get_chat_completion(
         # if hasattr(llm_wrapper, "supports_first_message") and llm_wrapper.supports_first_message:
         if hasattr(llm_wrapper, "supports_first_message"):
             prompt = llm_wrapper.chat_completion_to_prompt(
-                messages, functions, functions_documentation=documentation, first_message=first_message
+                messages, functions,  first_message=first_message
             )
         else:
             prompt = llm_wrapper.chat_completion_to_prompt(messages, functions)
