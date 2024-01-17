98836a80dc29473afa90d54be54954cd0e967514
Sarah Wooders <sarahwooders@gmail.com>
2024-01-16 20:37:58 -0800

    fix: Remove usage of `config.anon_clientid`  (#837)

--- memgpt/agent_store/db.py
+++ memgpt/agent_store/db.py
@@ -337,8 +337,8 @@ class SQLStorageConnector(StorageConnector):
             db_records = self.session.query(self.db_model).filter(*filters).all()
         return [record.to_record() for record in db_records]
 
-    def get(self, id: str) -> Optional[Record]:
-        db_record = self.session.query(self.db_model).get(id)
+    def get(self, id: uuid.UUID) -> Optional[Record]:
+        db_record = self.session.get(self.db_model, id)
         if db_record is None:
             return None
         return db_record.to_record()
