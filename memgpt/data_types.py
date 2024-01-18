ca5492dded6d6aec481939aaa3c982e6b2ee1353
cpacker <packercharles@gmail.com>
2024-01-18 15:31:42 -0800

    fixed default embedding endpoint address

--- memgpt/data_types.py
+++ memgpt/data_types.py
@@ -317,7 +317,7 @@ class EmbeddingConfig:
     def __init__(
         self,
         embedding_endpoint_type: Optional[str] = "openai",
-        embedding_endpoint: Optional[str] = "https://api.openai.com/v1/embeddings",
+        embedding_endpoint: Optional[str] = "https://api.openai.com/v1",
         embedding_model: Optional[str] = "text-embedding-ada-002",
         embedding_dim: Optional[int] = 1536,
         embedding_chunk_size: Optional[int] = 300,
