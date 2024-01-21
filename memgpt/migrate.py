b394270115976b33cd039d1106b2b34046c0d72b
cpacker <packercharles@gmail.com>
2024-01-20 22:04:03 -0800

    add creation of default user

--- memgpt/migrate.py
+++ memgpt/migrate.py
@@ -146,9 +146,14 @@ def migrate_source(source_name: str, data_dir: str = MEMGPT_DIR, ms: Optional[Me
     user_id = uuid.UUID(config.anon_clientid)
     user = ms.get_user(user_id=user_id)
     if user is None:
-        raise ValueError(
-            f"Failed to load user {str(user_id)} from database. Please make sure to migrate your config before migrating agents."
-        )
+        ms.create_user(User(id=user_id))
+        user = ms.get_user(user_id=user_id)
+        if user is None:
+            typer.secho(f"Failed to create default user in database.", fg=typer.colors.RED)
+            sys.exit(1)
+        # raise ValueError(
+        # f"Failed to load user {str(user_id)} from database. Please make sure to migrate your config before migrating agents."
+        # )
 
     # insert source into metadata store
     source = Source(user_id=user.id, name=source_name)
