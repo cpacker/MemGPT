10cfe4b016bad31384eba05e1a7b38a721cffbfa
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-01-22 16:33:22 +0100

    Update chatml.py

--- memgpt/local_llm/llm_chat_completion_wrappers/chatml.py
+++ memgpt/local_llm/llm_chat_completion_wrappers/chatml.py
@@ -102,7 +102,7 @@ class ChatMLInnerMonologueWrapper(LLMChatCompletionWrapper):
         prompt += "\n"
         if function_documentation is not None:
             prompt += f"Please select the most suitable function and parameters from the list of available functions below, based on the ongoing conversation. Provide your response in JSON format."
-            prompt += f"\nAvailable functions:"
+            prompt += f"\nAvailable functions:\n"
             prompt += function_documentation
         else:
             prompt += self._compile_function_block(functions)
