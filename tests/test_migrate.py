1ad7bb72e1b03da0aa5c40035cd8dfc135541dbd
Sarah Wooders <sarahwooders@gmail.com>
2024-01-22 17:14:27 -0800

    remove changes

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -1,6 +1,5 @@
 import os
 from memgpt.migrate import migrate_all_agents, migrate_all_sources
-<<<<<<< HEAD
 from memgpt import MemGPT
 from memgpt.config import MemGPTConfig
 from .utils import wipe_config
@@ -12,15 +11,6 @@ def test_migrate_0211():
     data_dir = "tests/data/memgpt-0.2.11"
     # os.environ["MEMGPT_CONFIG_PATH"] = os.path.join(data_dir, "config")
     # print(f"MEMGPT_CONFIG_PATH={os.environ['MEMGPT_CONFIG_PATH']}")
-=======
-
-
-def test_migrate_0211():
-
-    data_dir = "tests/data/memgpt-0.2.11"
-    os.environ["MEMGPT_CONFIG_PATH"] = os.path.join(data_dir, "config")
-    print(f"MEMGPT_CONFIG_PATH={os.environ['MEMGPT_CONFIG_PATH']}")
->>>>>>> f507207 (add migration test)
     res = migrate_all_agents(data_dir)
     assert res["failed_migrations"] == 0, f"Failed migrations: {res}"
     res = migrate_all_sources(data_dir)
