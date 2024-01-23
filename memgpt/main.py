5a5b6df947f116cda138ebf36237b960d1eb3c01
cpacker <packercharles@gmail.com>
2024-01-22 17:33:15 -0800

    correct unrelated attach errors found inside main.py attach cmd

--- memgpt/main.py
+++ memgpt/main.py
@@ -120,7 +120,6 @@ def run_agent_loop(memgpt_agent, config: MemGPTConfig, first, ms: MetadataStore,
                     # TODO: alternatively, only list sources with compatible embeddings, and print warning about non-compatible sources
 
                     data_source_options = ms.list_sources(user_id=memgpt_agent.agent_state.user_id)
-                    data_source_options = [s.name for s in data_source_options]
                     if len(data_source_options) == 0:
                         typer.secho(
                             'No sources available. You must load a souce with "memgpt load ..." before running /attach.',
@@ -133,7 +132,10 @@ def run_agent_loop(memgpt_agent, config: MemGPTConfig, first, ms: MetadataStore,
                     valid_options = []
                     invalid_options = []
                     for source in data_source_options:
-                        if source.embedding_model == memgpt_agent.embedding_model and source.embedding_dim == memgpt_agent.embedding_dim:
+                        if (
+                            source.embedding_model == memgpt_agent.agent_state.embedding_config.embedding_model
+                            and source.embedding_dim == memgpt_agent.agent_state.embedding_config.embedding_dim
+                        ):
                             valid_options.append(source.name)
                         else:
                             invalid_options.append(source.name)
@@ -148,7 +150,7 @@ def run_agent_loop(memgpt_agent, config: MemGPTConfig, first, ms: MetadataStore,
                     data_source = questionary.select("Select data source", choices=valid_options).ask()
 
                     # attach new data
-                    attach(memgpt_agent.config.name, data_source)
+                    attach(memgpt_agent.agent_state.name, data_source)
 
                     continue
 
