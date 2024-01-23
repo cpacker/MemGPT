e9f4ef1920ecabd468c955925306887640d37c24
Adam <scoggins.aj@gmail.com>
2024-01-23 16:04:45 -0600

    Ran prettier on all modified files for this PR

--- chatui/src/app/libs/humans/use-humans.query.ts
+++ chatui/src/app/libs/humans/use-humans.query.ts
@@ -3,16 +3,16 @@ import { API_BASE_URL } from '../constants';
 import { Human } from './human';
 
 export const useHumansQuery = (userId: string | null | undefined) =>
-    useQuery({
-        queryKey: [userId, 'humans', 'list'],
-        enabled: !!userId,
-        queryFn: async () => {
-            const response = await fetch(`${API_BASE_URL}/humans?user_id=${encodeURIComponent(userId || '')}`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok for fetching humans');
-            }
-            return (await response.json()) as Promise<{
-                humans: Human[];
-            }>;
-        },
-    });
+	useQuery({
+		queryKey: [userId, 'humans', 'list'],
+		enabled: !!userId,
+		queryFn: async () => {
+			const response = await fetch(`${API_BASE_URL}/humans?user_id=${encodeURIComponent(userId || '')}`);
+			if (!response.ok) {
+				throw new Error('Network response was not ok for fetching humans');
+			}
+			return (await response.json()) as Promise<{
+				humans: Human[];
+			}>;
+		},
+	});
--- chatui/src/app/libs/models/use-models.query.ts
+++ chatui/src/app/libs/models/use-models.query.ts
@@ -3,16 +3,16 @@ import { API_BASE_URL } from '../constants';
 import { Model } from './model';
 
 export const useModelsQuery = (userId: string | null | undefined) =>
-    useQuery({
-        queryKey: [userId, 'models', 'list'],
-        enabled: !!userId,
-        queryFn: async () => {
-            const response = await fetch(`${API_BASE_URL}/models?user_id=${encodeURIComponent(userId || '')}`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok for fetching models');
-            }
-            return (await response.json()) as Promise<{
-                models: Model[];
-            }>;
-        },
-    });
+	useQuery({
+		queryKey: [userId, 'models', 'list'],
+		enabled: !!userId,
+		queryFn: async () => {
+			const response = await fetch(`${API_BASE_URL}/models?user_id=${encodeURIComponent(userId || '')}`);
+			if (!response.ok) {
+				throw new Error('Network response was not ok for fetching models');
+			}
+			return (await response.json()) as Promise<{
+				models: Model[];
+			}>;
+		},
+	});
