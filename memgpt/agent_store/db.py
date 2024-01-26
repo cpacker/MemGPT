bab6d6fd76e8d37e23a8d27beec55c14f0ef6051
Adam <scoggins.aj@gmail.com>
2024-01-26 04:03:21 -0600

    Proper import

--- memgpt/agent_store/db.py
+++ memgpt/agent_store/db.py
@@ -1,4 +1,5 @@
 import os
+import base64
 from sqlalchemy import create_engine, Column, String, BIGINT, select, inspect, text, JSON, BLOB, BINARY, ARRAY, DateTime
 from sqlalchemy import func, or_, and_
 from sqlalchemy import desc, asc
