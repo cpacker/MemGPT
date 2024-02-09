f0dac7ea1985aaea78b86662f57180577d88512f
cpacker <packercharles@gmail.com>
2024-02-08 16:52:23 -0800

    patch test

--- tests/test_summarize.py
+++ tests/test_summarize.py
@@ -9,6 +9,8 @@ from .utils import wipe_config
 
 
 # test_agent_id = "test_agent"
+test_agent_name = f"test_client_{str(uuid.uuid4())}"
+test_user_id = uuid.uuid4()
 client = None
 agent_obj = None
 
@@ -22,9 +24,13 @@ def create_test_agent():
     else:
         client = MemGPT(quickstart="memgpt_hosted")
 
+    user = client.server.create_user({"id": test_user_id})
+    assert user is not None
+
     agent_state = client.create_agent(
         agent_config={
-            # "name": test_agent_id,
+            "user_id": test_user_id,
+            "name": test_agent_name,
             "persona": constants.DEFAULT_PERSONA,
             "human": constants.DEFAULT_HUMAN,
         }
@@ -32,8 +38,7 @@ def create_test_agent():
 
     global agent_obj
     config = MemGPTConfig.load()
-    user_id = uuid.UUID(config.anon_clientid)
-    agent_obj = client.server._get_or_load_agent(user_id=user_id, agent_id=agent_state.id)
+    agent_obj = client.server._get_or_load_agent(user_id=test_user_id, agent_id=agent_state.id)
 
 
 def test_summarize():
