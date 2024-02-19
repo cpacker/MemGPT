2fdf59595597cbe1d04e1ff0390b8dd948d5849c
Sarah Wooders <sarahwooders@gmail.com>
2024-02-18 16:49:57 -0800

    remove no longer supported functions, add skeleton for vectordb loading

--- memgpt/data_sources/connectors.py
+++ memgpt/data_sources/connectors.py
@@ -10,13 +10,10 @@ from llama_index.core import Document as LlamaIndexDocument
 
 
 class DataConnector:
-    def __init__(self, data_source):
-        self.data_source = data_source
-
-    def generate_documents(self) -> Iterator[Document]:
+    def generate_documents(self) -> Iterator[Tuple[str, Dict]]:  # -> Iterator[Document]:
         pass
 
-    def generate_passages(self, documents: List[Document]) -> Iterator[Passage]:
+    def generate_passages(self, documents: List[Document], chunk_size: int = 1024) -> Iterator[Tuple[str, Dict]]:  # -> Iterator[Passage]:
         pass
 
 
@@ -82,7 +79,7 @@ def load_data(
     return passage_count, document_count
 
 
-class DirectoryConnector:
+class DirectoryConnector(DataConnector):
     def __init__(self, input_files: List[str] = None, input_directory: str = None, recursive: bool = False, extensions: List[str] = None):
         self.connector_type = "directory"
         self.input_files = input_files
@@ -128,3 +125,72 @@ class DirectoryConnector:
                 #    doc_id=document.id,
                 # )
                 yield node.text, None
+
+
+class WebConnector(DataConnector):
+
+    # TODO
+
+    def __init__(self):
+        pass
+
+    def generate_documents(self) -> Iterator[Tuple[str, Dict]]:  # -> Iterator[Document]:
+        pass
+
+    def generate_passages(self, documents: List[Document], chunk_size: int = 1024) -> Iterator[Tuple[str, Dict]]:  # -> Iterator[Passage]:
+        pass
+
+
+class VectorDBConnector(DataConnector):
+
+    # NOTE: this class has not been properly tested, so is unlikely to work
+    # TODO: allow loading multiple tables (1:1 mapping between Document and Table)
+
+    def __init__(
+        self,
+        name: str,
+        uri: str,
+        table_name: str,
+        text_column: str,
+        embedding_column: str,
+        embedding_dim: int,
+    ):
+        self.name = name
+        self.uri = uri
+        self.table_name = table_name
+        self.text_column = text_column
+        self.embedding_column = embedding_column
+        self.embedding_dim = embedding_dim
+
+        # connect to db table
+        from sqlalchemy import create_engine
+
+        self.engine = create_engine(uri)
+
+    def generate_documents(self) -> Iterator[Tuple[str, Dict]]:  # -> Iterator[Document]:
+        yield self.table_name, None
+
+    def generate_passages(self, documents: List[Document], chunk_size: int = 1024) -> Iterator[Tuple[str, Dict]]:  # -> Iterator[Passage]:
+        from sqlalchemy import select, MetaData, Table, Inspector
+        from pgvector.sqlalchemy import Vector
+
+        metadata = MetaData()
+        # Create an inspector to inspect the database
+        inspector = Inspector.from_engine(self.engine)
+        table_names = inspector.get_table_names()
+        assert self.table_name in table_names, f"Table {self.table_name} not found in database: tables that exist {table_names}."
+
+        table = Table(self.table_name, metadata, autoload_with=self.engine)
+
+        # Prepare a select statement
+        select_statement = select(table.c[self.text_column], table.c[self.embedding_column].cast(Vector(self.embedding_dim)))
+
+        # Execute the query and fetch the results
+        # TODO: paginate results
+        with self.engine.connect() as connection:
+            result = connection.execute(select_statement).fetchall()
+
+        for text, embedding in result:
+            # assume that embeddings are the same model as in config
+            # TODO: don't re-compute embedding
+            yield text, {"embedding": embedding}
