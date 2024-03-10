329615f2c1a22ef9e39a827aa39c4a6153c6e4cb
Sarah Wooders <sarahwooders@gmail.com>
2024-03-10 16:40:54 -0700

    fix: fix bugs for retrieving archival memory via REST API + tests  (#1122)

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,6 +6,14 @@ from dotenv import load_dotenv
 
 from memgpt import Admin, create_client
 from memgpt.constants import DEFAULT_PRESET
+from dotenv import load_dotenv
+
+from tests.config import TestMGPTConfig
+
+from memgpt.credentials import MemGPTCredentials
+from memgpt.data_types import EmbeddingConfig, LLMConfig
+from .utils import wipe_config, wipe_memgpt_home
+
 
 import pytest
 import uuid
@@ -28,6 +36,63 @@ test_server_token = "test_server_token"
 def run_server():
     import uvicorn
     from memgpt.server.rest_api.server import app
+
+    load_dotenv()
+
+    # Use os.getenv with a fallback to os.environ.get
+    db_url = os.getenv("PGVECTOR_TEST_DB_URL") or os.environ.get("PGVECTOR_TEST_DB_URL")
+    assert db_url, "Missing PGVECTOR_TEST_DB_URL"
+
+    if os.getenv("OPENAI_API_KEY"):
+        config = TestMGPTConfig(
+            archival_storage_uri=db_url,
+            recall_storage_uri=db_url,
+            metadata_storage_uri=db_url,
+            archival_storage_type="postgres",
+            recall_storage_type="postgres",
+            metadata_storage_type="postgres",
+            # embeddings
+            default_embedding_config=EmbeddingConfig(
+                embedding_endpoint_type="openai",
+                embedding_endpoint="https://api.openai.com/v1",
+                embedding_dim=1536,
+            ),
+            # llms
+            default_llm_config=LLMConfig(
+                model_endpoint_type="openai",
+                model_endpoint="https://api.openai.com/v1",
+                model="gpt-4",
+            ),
+        )
+        credentials = MemGPTCredentials(
+            openai_key=os.getenv("OPENAI_API_KEY"),
+        )
+    else:  # hosted
+        config = TestMGPTConfig(
+            archival_storage_uri=db_url,
+            recall_storage_uri=db_url,
+            metadata_storage_uri=db_url,
+            archival_storage_type="postgres",
+            recall_storage_type="postgres",
+            metadata_storage_type="postgres",
+            # embeddings
+            default_embedding_config=EmbeddingConfig(
+                embedding_endpoint_type="hugging-face",
+                embedding_endpoint="https://embeddings.memgpt.ai",
+                embedding_model="BAAI/bge-large-en-v1.5",
+                embedding_dim=1024,
+            ),
+            # llms
+            default_llm_config=LLMConfig(
+                model_endpoint_type="vllm",
+                model_endpoint="https://api.memgpt.ai",
+                model="ehartford/dolphin-2.5-mixtral-8x7b",
+            ),
+        )
+        credentials = MemGPTCredentials()
+
+    config.save()
+    credentials.save()
 
     uvicorn.run(app, host="localhost", port=8283, log_level="info")
 
@@ -124,8 +189,14 @@ def test_sources(client, agent):
     print("listed sources", sources)
     assert len(sources) == 1
 
+    # check agent archival memory size
+    archival_memories = client.get_agent_archival_memory(agent_id=agent.id)
+    print(archival_memories)
+    assert len(archival_memories) == 0
+
     # load a file into a source
     filename = "CONTRIBUTING.md"
+    num_passages = 20
     response = client.load_file_into_source(filename, source.id)
     print(response)
 
@@ -133,7 +204,10 @@ def test_sources(client, agent):
     # TODO: make sure things run in the right order
     client.attach_source_to_agent(source_name="test_source", agent_id=agent.id)
 
-    # TODO: list archival memory
+    # list archival memory
+    archival_memories = client.get_agent_archival_memory(agent_id=agent.id)
+    print(archival_memories)
+    assert len(archival_memories) == num_passages
 
     # detach the source
     # TODO: add when implemented
