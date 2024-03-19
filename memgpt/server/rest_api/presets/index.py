3065923b8a389f34076d1116ab28017cdbbf0393
cpacker <packercharles@gmail.com>
2024-03-18 18:30:07 -0700

    update the POST request fields to match the PresetModel (with some fields dropped)

--- memgpt/server/rest_api/presets/index.py
+++ memgpt/server/rest_api/presets/index.py
@@ -1,6 +1,6 @@
 import uuid
 from functools import partial
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 from fastapi import APIRouter, Body, Depends, Query, HTTPException, status
 from fastapi.responses import JSONResponse
@@ -10,6 +10,8 @@ from memgpt.models.pydantic_models import PresetModel
 from memgpt.server.rest_api.auth_token import get_current_user
 from memgpt.server.rest_api.interface import QueuingInterface
 from memgpt.server.server import SyncServer
+from memgpt.constants import DEFAULT_HUMAN, DEFAULT_PERSONA
+from memgpt.utils import get_human_text, get_persona_text
 
 router = APIRouter()
 
@@ -27,8 +29,16 @@ class ListPresetsResponse(BaseModel):
 
 
 class CreatePresetsRequest(BaseModel):
-    preset: PresetModel = Field(..., description="The name of the preset.")
+    # TODO is there a cleaner way to create the request from the PresetModel (need to drop fields though)?
+    name: str = Field(..., description="The name of the preset.")
+    # id: uuid.UUID = Field(default_factory=uuid.uuid4, description="The unique identifier of the preset.")
+    # user_id: uuid.UUID = Field(..., description="The unique identifier of the user who created the preset.")
     description: Optional[str] = Field(None, description="The description of the preset.")
+    # created_at: datetime = Field(default_factory=datetime.now, description="The unix timestamp of when the preset was created.")
+    system: str = Field(..., description="The system prompt of the preset.")
+    persona: str = Field(default=get_persona_text(DEFAULT_PERSONA), description="The persona of the preset.")
+    human: str = Field(default=get_human_text(DEFAULT_HUMAN), description="The human of the preset.")
+    functions_schema: List[Dict] = Field(..., description="The functions schema of the preset.")
 
 
 class CreateSourceResponse(BaseModel):
@@ -61,7 +71,7 @@ def setup_presets_index_router(server: SyncServer, interface: QueuingInterface, 
     ):
         """Create a preset."""
         try:
-            new_preset = PresetModel(**vars(request))
+            new_preset = PresetModel(**vars(request).update(user_id=user_id))
             server.ms.create_preset(preset=new_preset)
             return new_preset
         except HTTPException:
