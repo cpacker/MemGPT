158f9089a65132a696c2a34dc0b9d759e315f7b5
Sarah Wooders <sarahwooders@gmail.com>
2024-03-22 14:50:27 -0700

    use env was database uri

--- init.sql
+++ init.sql
@@ -2,25 +2,26 @@
 
 -- Fetch the docker secrets, if they are available.
 -- Otherwise fall back to environment variables, or hardwired 'memgpt'
-\set db_user `([ -r /var/run/secrets/memgpt-user ] && cat /var/run/secrets/memgpt-user) || echo "${MEMGPT_USER:-memgpt}"`
-\set db_password `([ -r /var/run/secrets/memgpt-password ] && cat /var/run/secrets/memgpt-password) || echo "${MEMGPT_PASSWORD:-memgpt}"`
-\set db_name `([ -r /var/run/secrets/memgpt-db ] && cat /var/run/secrets/memgpt-db) || echo "${MEMGPT_DB:-memgpt}"`
+\set db_user `([ -r /var/run/secrets/memgpt-user ] && cat /var/run/secrets/memgpt-user) || echo "${POSTGRES_USER:-memgpt}"`
+\set db_password `([ -r /var/run/secrets/memgpt-password ] && cat /var/run/secrets/memgpt-password) || echo "${POSTGRES_PASSWORD:-memgpt}"`
+\set db_name `([ -r /var/run/secrets/memgpt-db ] && cat /var/run/secrets/memgpt-db) || echo "${POSTGRES_DB:-memgpt}"`
 
-CREATE USER :"db_user"
-    WITH PASSWORD :'db_password'
-    NOCREATEDB
-    NOCREATEROLE
-    ;
 
-CREATE DATABASE :"db_name"
-    WITH
-    OWNER = :"db_user"
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+-- CREATE USER :"db_user"
+--     WITH PASSWORD :'db_password'
+--     NOCREATEDB
+--     NOCREATEROLE
+--     ;
+--
+-- CREATE DATABASE :"db_name"
+--     WITH
+--     OWNER = :"db_user"
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'en_US.utf8'
+--     LC_CTYPE = 'en_US.utf8'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1;
 
 -- Set up our schema and extensions in our new database.
 \c :"db_name"
