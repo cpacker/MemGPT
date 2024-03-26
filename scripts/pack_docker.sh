ff716e65f06acc636f320f27de703447ae1688d2
Sarah Wooders <sarahwooders@gmail.com>
2024-03-26 16:01:25 -0700

    remove docker postgres

--- scripts/pack_docker.sh
+++ scripts/pack_docker.sh
@@ -1,4 +1,4 @@
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
 export MEMGPT_VERSION=$(memgpt version)
-docker build -t memgpt/memgpt-server:${MEMGPT_VERSION} .
+#docker build -t memgpt/memgpt-server:${MEMGPT_VERSION} --platform linux/x86_64 .
+docker buildx build --platform=linux/amd64,linux/arm64,linux/x86_64 -t memgpt/memgpt-server:${MEMGPT_VERSION} .
 docker push memgpt/memgpt-server:${MEMGPT_VERSION}
