162b1925c14c6d57747ef86f9e4a4c6a918e6aaa
Sarah Wooders <sarahwooders@gmail.com>
2024-03-25 22:32:26 -0700

    feat: create memgpt server with postgres DB with `docker compose up`  (#1183)

--- db/run_postgres.sh
+++ db/run_postgres.sh
@@ -4,7 +4,7 @@ docker build -f db/Dockerfile.simple -t pg-test .
 # run container
 docker run -d --rm \
    --name memgpt-db-test \
-   -p 8888:5432 \
+   -p 5432:5432 \
    -e POSTGRES_PASSWORD=password \
    -v memgpt_db_test:/var/lib/postgresql/data \
     pg-test:latest
