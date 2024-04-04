fd1560e9c549336b4e4e0c5c07cc1c07e90400f0
Charles Packer <packercharles@gmail.com>
2024-04-03 18:59:43 -0700

    feat: add dev portal (#1191)

--- chatui/libs/components/typography/src/libs/h1.ts
+++
@@ -1,5 +0,0 @@
-import { cn } from '@memgpt/utils';
-import { ClassValue } from 'clsx/clsx';
-
-export const cnH1 = (...inputs: ClassValue[]) =>
-	cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl', inputs);
--- chatui/src/app/libs/auth/use-auth.query.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { API_BASE_URL } from '../constants';
-
-export type AuthResponse = { uuid: string };
-export const useAuthQuery = () =>
-	useQuery({
-		queryKey: ['auth'],
-		queryFn: async () => (await fetch(API_BASE_URL + `/auth`).then((res) => res.json())) as Promise<AuthResponse>,
-	});
