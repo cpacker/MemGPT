fe71bb8c4e878162be27290ef5614e177cf6299a
Ethan Knox <ethan@staticdata.io>
2024-04-10 08:21:51 -0400

    development/release builder modes for docker image

--- scripts/pack_docker.sh
+++ scripts/pack_docker.sh
@@ -1,3 +1,3 @@
 export MEMGPT_VERSION=$(memgpt version)
-docker buildx build --platform=linux/amd64,linux/arm64,linux/x86_64 --build-arg MEMGPT_ENVIRONMENT=PRODUCTION -t memgpt/memgpt-server:${MEMGPT_VERSION} .
+docker buildx build --platform=linux/amd64,linux/arm64,linux/x86_64 --build-arg MEMGPT_ENVIRONMENT=RELEASE -t memgpt/memgpt-server:${MEMGPT_VERSION} .
 docker push memgpt/memgpt-server:${MEMGPT_VERSION}
