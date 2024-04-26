ca0047439461ecc41807c5effc3921a218510a5c
Tom Bedor <tombedor@gmail.com>
2024-04-26 14:52:45 -0700

    add created_at to archival

--- memgpt/agent_store/db.py
+++ memgpt/agent_store/db.py
@@ -107,6 +107,7 @@ def get_db_model(
             embedding_model = Column(String)
 
             metadata_ = Column(MutableJson)
+            created_at = Column(DateTime(timezone=True), server_default=func.now())
 
             def __repr__(self):
                 return f"<Passage(passage_id='{self.id}', text='{self.text}', embedding='{self.embedding})>"
