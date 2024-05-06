ee16a8b7e35584c3ba096c6e8964b9fc9fe688af
neverbiasu <1751162157@qq.com>
2024-05-06 23:04:07 +0800

    revise

--- memgpt/llm_api/openai.py
+++ memgpt/llm_api/openai.py
@@ -193,11 +193,11 @@ def openai_chat_completions_process_stream(
 
                     for tool_call_delta in tool_calls_delta:
                         if tool_call_delta.id is not None:
-                            assert accum_message.tool_calls[tool_call_delta.index].id is None, "Overwriting existing data"
+                            assert accum_message.tool_calls[tool_call_delta.index].id == tool_call_delta.id, "Overwriting with different data"
                             accum_message.tool_calls[tool_call_delta.index].id = tool_call_delta.id
                         if tool_call_delta.function is not None:
                             if tool_call_delta.function.name is not None:
-                                assert accum_message.tool_calls[tool_call_delta.index].id is None, "Overwriting existing data"
+                                assert accum_message.tool_calls[tool_call_delta.index].id == tool_call_delta.id, "Overwriting with different data"
                                 accum_message.tool_calls[tool_call_delta.index].function.name = tool_call_delta.function.name
                             if tool_call_delta.function.arguments is not None:
                                 accum_message.tool_calls[tool_call_delta.index].function.arguments += tool_call_delta.function.arguments
