70f113cfaba844c97f3256af86448718a28552ec
Sarah Wooders <sarahwooders@gmail.com>
2024-05-13 12:05:32 -0700

    feat: add more tool functionality for python client  (#1361)
    Co-authored-by: cpacker <packercharles@gmail.com>

--- memgpt/server/rest_api/tools/index.py
+++ memgpt/server/rest_api/tools/index.py
@@ -1,6 +1,6 @@
 from typing import List, Literal, Optional
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, HTTPException
 from pydantic import BaseModel, Field
 
 from memgpt.models.pydantic_models import ToolModel
@@ -28,6 +28,33 @@ class CreateToolResponse(BaseModel):
 def setup_tools_index_router(server: SyncServer, interface: QueuingInterface, password: str):
     # get_current_user_with_server = partial(partial(get_current_user, server), password)
 
+    @router.delete("/tools/{tool_name}", tags=["tools"])
+    async def delete_tool(
+        tool_name: str,
+        # user_id: uuid.UUID = Depends(get_current_user_with_server), # TODO: add back when user-specific
+    ):
+        """
+        Delete a tool by name
+        """
+        # Clear the interface
+        interface.clear()
+        # tool = server.ms.delete_tool(user_id=user_id, tool_name=tool_name) TODO: add back when user-specific
+        server.ms.delete_tool(name=tool_name)
+
+    @router.get("/tools/{tool_name}", tags=["tools"], response_model=ToolModel)
+    async def get_tool(tool_name: str):
+        """
+        Get a tool by name
+        """
+        # Clear the interface
+        interface.clear()
+        # tool = server.ms.get_tool(user_id=user_id, tool_name=tool_name) TODO: add back when user-specific
+        tool = server.ms.get_tool(tool_name=tool_name)
+        if tool is None:
+            # return 404 error
+            raise HTTPException(status_code=404, detail=f"Tool with name {tool_name} not found.")
+        return tool
+
     @router.get("/tools", tags=["tools"], response_model=ListToolsResponse)
     async def list_all_tools(
         # user_id: uuid.UUID = Depends(get_current_user_with_server), # TODO: add back when user-specific
@@ -41,7 +68,7 @@ def setup_tools_index_router(server: SyncServer, interface: QueuingInterface, pa
         tools = server.ms.list_tools()
         return ListToolsResponse(tools=tools)
 
-    @router.post("/tools", tags=["tools"], response_model=CreateToolResponse)
+    @router.post("/tools", tags=["tools"], response_model=ToolModel)
     async def create_tool(
         request: CreateToolRequest = Body(...),
         # user_id: uuid.UUID = Depends(get_current_user_with_server), # TODO: add back when user-specific
@@ -49,20 +76,26 @@ def setup_tools_index_router(server: SyncServer, interface: QueuingInterface, pa
         """
         Create a new tool (dummy route)
         """
-        from memgpt.functions.functions import write_function
+        from memgpt.functions.functions import load_function_file, write_function
 
         # check if function already exists
         if server.ms.get_tool(request.name):
             raise ValueError(f"Tool with name {request.name} already exists.")
 
         # write function to ~/.memgt/functions directory
-        write_function(request.name, request.name, request.source_code)
+        file_path = write_function(request.name, request.name, request.source_code)
+
+        # TODO: Use load_function_file to load function schema
+        schema = load_function_file(file_path)
+        assert len(list(schema.keys())) == 1, "Function schema must have exactly one key"
+        json_schema = list(schema.values())[0]["json_schema"]
 
         print("adding tool", request.name, request.tags, request.source_code)
-        tool = ToolModel(name=request.name, json_schema={}, tags=request.tags, source_code=request.source_code)
+        tool = ToolModel(name=request.name, json_schema=json_schema, tags=request.tags, source_code=request.source_code)
+        tool.id
         server.ms.add_tool(tool)
 
         # TODO: insert tool information into DB as ToolModel
-        return CreateToolResponse(tool=ToolModel(name=request.name, json_schema={}, tags=[], source_code=request.source_code))
+        return server.ms.get_tool(request.name)
 
     return router
