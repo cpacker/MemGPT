7c5af3286e91e124a2b2e7f0d8c317d8a5b30ffb
Eren-Ajani Tshimanga <eren.tish@gmail.com>
2024-06-09 16:13:03 -0700

    reduced page_size and removed clean up fixture in pagination tests causing max connection error

--- tests/test_admin_client.py
+++ tests/test_admin_client.py
@@ -33,21 +33,6 @@ def admin_client():
     yield admin
 
 
-@pytest.fixture(scope="session", autouse=True)
-def before_cleanup():
-
-    # Clean up before tests run
-    print("Running setup code before tests...")
-    clean_up_users_and_keys()
-
-    # yield control to test
-    yield
-
-    # Clean up after tests run
-    print("Running clean up after tests...")
-    clean_up_users_and_keys()
-
-
 def test_admin_client(admin_client):
     _reset_config()
 
@@ -99,27 +84,29 @@ def test_admin_client(admin_client):
 def test_get_users_pagination(admin_client):
     _reset_config()
 
-    page_size = 50
-    num_users = 56
+    page_size = 5
+    num_users = 7
     expected_users_remainder = num_users - page_size
 
     # create users
+    all_user_ids = []
     for i in range(num_users):
 
         user_id = uuid.uuid4()
+        all_user_ids.append(user_id)
         key_name = "test_key" + f"{i}"
 
         create_user_response = admin_client.create_user(user_id)
         admin_client.create_key(create_user_response.user_id, key_name)
 
     # list users in page 1
-    get_all_users_response1 = admin_client.get_users()
+    get_all_users_response1 = admin_client.get_users(limit=page_size)
     cursor1 = get_all_users_response1.cursor
     user_list1 = get_all_users_response1.user_list
     assert len(user_list1) == page_size
 
     # list users in page 2 using cursor
-    get_all_users_response2 = admin_client.get_users(cursor1)
+    get_all_users_response2 = admin_client.get_users(cursor1, limit=page_size)
     cursor2 = get_all_users_response2.cursor
     user_list2 = get_all_users_response2.user_list
 
@@ -127,23 +114,19 @@ def test_get_users_pagination(admin_client):
     assert cursor1 != cursor2
 
     # delete users
-    clean_up_users_and_keys()
+    clean_up_users_and_keys(all_user_ids)
 
     # list users to check pagination with no users
     users = admin_client.get_users()
     assert len(users.user_list) == 0, f"Expected 0 users, got {users}"
 
 
-def clean_up_users_and_keys():
+def clean_up_users_and_keys(user_id_list):
     admin_client = Admin(test_base_url, test_server_token)
-    cursor = None
 
     # clean up all keys and users
-    while admin_client.get_users(cursor).user_list:
-        users = admin_client.get_users(cursor)
-        cursor = admin_client.get_users(cursor).cursor
-        for user_id in [uuid.UUID(u["user_id"]) for u in users.user_list]:
-            keys_list = admin_client.get_keys(user_id)
-            for key in keys_list:
-                admin_client.delete_key(key)
-            admin_client.delete_user(user_id)
+    for user_id in user_id_list:
+        keys_list = admin_client.get_keys(user_id)
+        for key in keys_list:
+            admin_client.delete_key(key)
+        admin_client.delete_user(user_id)
