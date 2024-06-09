9bd18ad9e8ac895d671fcab327f2639212a954e9
Sarah Wooders <sarahwooders@gmail.com>
2024-06-09 14:51:53 -0700

    fix summary tests

--- tests/test_summarize.py
+++ tests/test_summarize.py
@@ -47,25 +47,25 @@ def test_summarize():
     response = client.user_message(
         agent_id=agent_obj.agent_state.id,
         message="Hey, how's it going? What do you think about this whole shindig",
-    )
+    ).messages
     assert response is not None and len(response) > 0
     print(f"test_summarize: response={response}")
 
     response = client.user_message(
         agent_id=agent_obj.agent_state.id,
         message="Any thoughts on the meaning of life?",
-    )
+    ).messages
     assert response is not None and len(response) > 0
     print(f"test_summarize: response={response}")
 
-    response = client.user_message(agent_id=agent_obj.agent_state.id, message="Does the number 42 ring a bell?")
+    response = client.user_message(agent_id=agent_obj.agent_state.id, message="Does the number 42 ring a bell?").messages
     assert response is not None and len(response) > 0
     print(f"test_summarize: response={response}")
 
     response = client.user_message(
         agent_id=agent_obj.agent_state.id,
         message="Would you be surprised to learn that you're actually conversing with an AI right now?",
-    )
+    ).messages
     assert response is not None and len(response) > 0
     print(f"test_summarize: response={response}")
 
