7cae7cf58b9cd729cb4db0cdeb1be8a321cf8b06
Ethan Knox <ethan@Ethans-MacBook-Pro.local>
2024-06-24 17:41:14 -0400

    I'm trying to keep this as close to the current model as possible while stripping out extraneous elements. The memory thing needs to be abstracted in a later time, never clear if these are strings or templates or references to a related object

--- memgpt/orm/agent.py
+++ memgpt/orm/agent.py
@@ -1,14 +1,25 @@
-from typing import Optional
+from typing import Optional, List, TYPE_CHECKING
 from sqlalchemy import String
 from sqlachemy.orm import Mapped, mapped_column, relationship
 
 from memgpt.orm.sqlalchemy_base import SqlalchemyBase
 from memgpt.orm.mixins import OrganizationMixin
+if TYPE_CHECKING:
+    from memgpt.orm.organization import Organization
+    from memgpt.orm.user import User
 
-class Agent(SqlalchemyBase):
+class Agent(SqlalchemyBase, OrganizationMixin):
     __tablename__ = 'agent'
 
-    name:Mapped[Optional[str]] = mapped_column(String, nullable=True, doc="a name to identify the token")
+    name:Mapped[Optional[str]] = mapped_column(String, nullable=True, doc="a human-readable identifier for an agent, non-unique.")
+    persona: Mapped[str] = mapped_column(doc="the persona text for the agent, current state.")
+    # todo: this doesn't allign with 1:M agents to users!
+    human: Mapped[str] = mapped_column(doc="the human text for the agent and the current user, current state.")
+    preset: Mapped[str] = mapped_column(doc="the preset text for the agent, current state.")
 
-    # is this correct? do agents belong to a single user?
-    user: Mapped["User"] = relationship("User", back_populates="agents")
+    # relationships
+    organization: Mapped["Organization"] = relationship("Organization", back_populates="agents")
+    users: Mapped[List["User"]] = relationship("User",
+                                               back_populates="agents",
+                                               secondary="user_agent",
+                                               doc="the users associated with this agent.")
