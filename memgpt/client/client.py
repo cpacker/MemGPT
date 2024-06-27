6b7af7f4b189762d6a6df33e31899b769e944789
Sarah Wooders <sarahwooders@gmail.com>
2024-06-26 20:14:51 -0700

    fix

--- memgpt/client/client.py
+++ memgpt/client/client.py
@@ -745,11 +745,11 @@ class LocalClient(AbstractClient):
 
     def user_message(self, agent_id: str, message: str) -> Union[List[Dict], Tuple[List[Dict], int]]:
         self.interface.clear()
-        self.server.user_message(user_id=self.user_id, agent_id=agent_id, message=message)
+        usage = self.server.user_message(user_id=self.user_id, agent_id=agent_id, message=message)
         if self.auto_save:
             self.save()
         else:
-            return UserMessageResponse(messages=self.interface.to_list())
+            return UserMessageResponse(messages=self.interface.to_list(), usage=usage)
 
     def run_command(self, agent_id: str, command: str) -> Union[str, None]:
         self.interface.clear()
