6a94991bc762420e4feb11816835c07eae07f350
Sarah Wooders <sarahwooders@gmail.com>
2024-06-30 11:52:44 -0700

    read db from config

--- scripts/migrate_0.3.17.py
+++ scripts/migrate_0.3.17.py
@@ -7,7 +7,11 @@ from memgpt.presets.presets import add_default_tools
 from memgpt.prompts import gpt_system
 
 # Replace this with your actual database connection URL
-DATABASE_URL = "sqlite:////Users/sarahwooders/.memgpt/sqlite.db"
+config = MemGPTConfig.load()
+if config.recall_storage_type == "sqlite":
+    DATABASE_URL = config.recall_storage_path
+else:
+    DATABASE_URL = config.recall_storage_uri
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
@@ -36,7 +40,6 @@ tool_model = Table("toolmodel", metadata, autoload_with=engine)
 tool_model.drop(engine)
 
 # re-create tables and add default tools
-config = MemGPTConfig.load()
 ms = MetadataStore(config)
 add_default_tools(None, ms)
 print("Tools", [tool.name for tool in ms.list_tools()])
