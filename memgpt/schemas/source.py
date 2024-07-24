f6db2788b8a948fcc3a211e2f9775039fa232f88
Sarah Wooders <sarahwooders@gmail.com>
2024-07-24 14:20:42 -0700

    add memgpt base

--- memgpt/schemas/source.py
+++ memgpt/schemas/source.py
@@ -1,30 +1,31 @@
-import uuid
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import Field
 
 from memgpt.schemas.embedding_config import EmbeddingConfig
+from memgpt.schemas.memgpt_base import MemGPTBase
 
 
-class Source(BaseModel):
-    source_id: uuid.UUID = Field(..., description="The id of the source.")
-    name: str = Field(..., description="The name of the source.")
+class BaseSource(MemGPTBase):
+    """
+    Shared attributes accourss all source schemas.
+    """
+
+    __id_prefix__ = "source"
     description: Optional[str] = Field(None, description="The description of the source.")
     embedding_config: Optional[EmbeddingConfig] = Field(None, description="The embedding configuration used by the passage.")
     # NOTE: .metadata is a reserved attribute on SQLModel
     metadata_: Optional[dict] = Field(None, description="Metadata associated with the source.")
 
 
-class SourceUpdate(BaseModel):
-    source_id: uuid.UUID = Field(..., description="The id of the source.")
-    name: Optional[str] = Field(None, description="The name of the source.")
-    description: Optional[str] = Field(None, description="The description of the source.")
-    embedding_config: Optional[EmbeddingConfig] = Field(None, description="The embedding configuration used by the passage.")
-    metadata_: Optional[dict] = Field(None, description="Metadata associated with the source.")
-
-
-class SourceCreate(BaseModel):
+class SourceCreate(BaseSource):
     name: str = Field(..., description="The name of the source.")
-    description: Optional[str] = Field(None, description="The description of the source.")
-    embedding_config: Optional[EmbeddingConfig] = Field(None, description="The embedding configuration used by the passage.")
-    metadata_: Optional[dict] = Field(None, description="Metadata associated with the source.")
+
+
+class Source(BaseSource):
+    id: str = MemGPTBase.generate_id_field()
+    name: str = Field(..., description="The name of the source.")
+
+
+class SourceUpdate(BaseSource):
+    name: Optional[str] = Field(None, description="The name of the source.")
