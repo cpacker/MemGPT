ce67696fadf09da067df0220156f8db47cf48224
Sarah Wooders <sarahwooders@gmail.com>
2024-07-25 13:57:49 -0700

    disable parallel tool calling

--- memgpt/llm_api/openai.py
+++ memgpt/llm_api/openai.py
@@ -370,6 +370,7 @@ def openai_chat_completions_request(
     url = smart_urljoin(url, "chat/completions")
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
     data = chat_completion_request.model_dump(exclude_none=True)
+    data["parallel_tool_calls"] = False
 
     printd("Request:\n", json.dumps(data, indent=2))
 
