37d962172106a633cbe4cbd94a6b00cbc1eb6b1b
cpacker <packercharles@gmail.com>
2024-08-04 19:11:44 -0700

    refactor: refactor memory.py so avoid direct access of memory.memory (use getters/setters), additionally patch bug derived from not returning a deepcopy of AgentState

--- memgpt/schemas/memory.py
+++ memgpt/schemas/memory.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional, Union
 
 from pydantic import BaseModel, Field
 
@@ -8,6 +8,7 @@ from memgpt.schemas.block import Block
 class Memory(BaseModel, validate_assignment=True):
     """Represents the in-context memory of the agent"""
 
+    # Private variable to avoid assignments with incorrect types
     memory: Dict[str, Block] = Field(default_factory=dict, description="Mapping from memory block section to memory block.")
 
     @classmethod
@@ -29,17 +30,52 @@ class Memory(BaseModel, validate_assignment=True):
         """Convert to dictionary representation"""
         return {key: value.dict() for key, value in self.memory.items()}
 
+    def to_flat_dict(self):
+        """Convert to a dictionary that maps directly from block names to values"""
+        return {k: v.value for k, v in self.memory.items() if v is not None}
+
+    def list_block_names(self) -> List[str]:
+        """Return a list of the block names held inside the memory object"""
+        return list(self.memory.keys())
+
+    # TODO we could also overload '__getitem__(self, key)'
+    def get_block(self, name: str) -> Block:
+        """Correct way to index into the memory.memory field, returns a Block"""
+        if name not in self.memory:
+            return KeyError(f"Block field {name} does not exist (available sections = {', '.join(list(self.memory.keys()))})")
+        else:
+            return self.memory[name]
+
+    # TODO we could also overload '__setitem__(self, key)'
+    def link_block(self, name: str, block: Block, override: Optional[bool] = False):
+        """Link a new block to the memory object"""
+        if not isinstance(block, Block):
+            raise ValueError(f"Param block must be type Block (not {type(block)})")
+        if not isinstance(name, str):
+            raise ValueError(f"Name must be str (not type {type(name)})")
+        if not override and name in self.memory:
+            raise ValueError(f"Block with name {name} already exists")
+
+        self.memory[name] = block
+
+    def update_block_value(self, name: str, value: Union[List[str], str]):
+        """Update the value of a block"""
+        if name not in self.memory:
+            raise ValueError(f"Block with name {name} does not exist")
+        if not (isinstance(value, str) or (isinstance(value, list) and all(isinstance(v, str) for v in value))):
+            raise ValueError(f"Provided value must be a string or list of strings")
+
+        self.memory[name].value = value
+
 
 class ChatMemory(Memory):
 
     def __init__(self, persona: str, human: str, limit: int = 2000):
         super().__init__()
-        self.memory = {
-            "persona": Block(name="persona", value=persona, limit=limit),
-            "human": Block(name="human", value=human, limit=limit),
-        }
+        self.link_block(name="persona", block=Block(name="persona", value=persona, limit=limit))
+        self.link_block(name="human", block=Block(name="human", value=human, limit=limit))
 
-    def core_memory_append(self, name: str, content: str) -> Optional[str]:
+    def corememory_append(self, name: str, content: str) -> Optional[str]:
         """
         Append to the contents of core memory.
 
@@ -50,10 +86,12 @@ class ChatMemory(Memory):
         Returns:
             Optional[str]: None is always returned as this function does not produce a response.
         """
-        self.memory[name].value += "\n" + content
+        current_value = str(self.get_block(name).value)
+        new_value = current_value + "\n" + str(content)
+        self.update_block_value(name=name, value=new_value)
         return None
 
-    def core_memory_replace(self, name: str, old_content: str, new_content: str) -> Optional[str]:
+    def corememory_replace(self, name: str, old_content: str, new_content: str) -> Optional[str]:
         """
         Replace the contents of core memory. To delete memories, use an empty string for new_content.
 
@@ -65,7 +103,9 @@ class ChatMemory(Memory):
         Returns:
             Optional[str]: None is always returned as this function does not produce a response.
         """
-        self.memory[name].value = self.memory[name].value.replace(old_content, new_content)
+        current_value = str(self.get_block(name).value)
+        new_value = current_value.replace(str(old_content), str(new_content))
+        self.update_block_value(name=name, value=new_value)
         return None
 
 
