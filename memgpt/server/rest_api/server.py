bcac9cce7e12e72a9ec77a7e567f02986c315f5b
Carlo <284474102@qq.com>
2024-08-06 11:34:12 +0800

    add http_exception_handler

--- memgpt/server/rest_api/server.py
+++ memgpt/server/rest_api/server.py
@@ -97,19 +97,13 @@ async def http_exception_handler(request: Request, exc: HTTPException) -> Respon
     url = request.url
     query_params = request.query_params
     logger_exception.error(
-        f" URL: {url}"
-        f" Method: {method}"
-        f" HTTP: {exc.status_code}"
-        f" Query Params: {query_params}"
-        f" ERROR triggered: {exc.detail}"
+        f" URL: {url}" f" Method: {method}" f" HTTP: {exc.status_code}" f" Query Params: {query_params}" f" ERROR triggered: {exc.detail}"
     )
 
     headers = getattr(exc, "headers", None)
     if not is_body_allowed_for_status_code(exc.status_code):
         return Response(status_code=exc.status_code, headers=headers)
-    return JSONResponse(
-        {"detail": exc.detail}, status_code=exc.status_code, headers=headers
-    )
+    return JSONResponse({"detail": exc.detail}, status_code=exc.status_code, headers=headers)
 
 
 app.add_middleware(
