8081ef19368a06b4633d95142f0dba7c7b84cad9
Ethan Knox <ethan@knox.dev>
2024-08-14 11:39:04 -0400

    smoke examples

---
+++ tests/smoke/test_cli_smoke.py
@@ -0,0 +1,39 @@
+from pytest import mark as m
+
+@m.describe("When interacting with MemGPT via the command line tool")
+class TestCLIClient:
+
+    @m.context("and this is a user's first time using the tool")
+    @m.it("should walk the user through setup of MemGPT")
+    async def test_cli_initial_setup(self):
+        pass
+
+    @m.context("and the user has already set up MemGPT")
+    @m.it("should direct the user to select an existing agent or create a new one")
+    async def test_cli_select_or_create_agent(self):
+        pass
+
+    @m.context("and the user has selected an existing agent")
+    @m.it("should allow the user to interact with the agent")
+    async def test_cli_interact_with_existing_agent(self):
+        pass
+
+    @m.context("and the user has created a new agent")
+    @m.it("should allow the user to interact with the agent")
+    async def test_cli_interact_with_new_agent(self):
+        pass
+
+    @m.context("and the user has selected an existing agent")
+    @m.it("should allow the user to rename the agent")
+    async def test_cli_rename_agent(self):
+        pass
+
+    @m.context("and the user has selected an existing agent")
+    @m.it("should allow the user to delete the agent")
+    async def test_cli_delete_agent(self):
+        pass
+
+    @m.context("and the user has created a new agent")
+    @m.it("should allow the user to persist (save) the agent")
+    async def test_cli_persist_agent(self):
+        pass
