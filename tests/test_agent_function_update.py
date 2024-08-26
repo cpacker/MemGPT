f4823515d3e00ea1ff5aa273c0532c96f42a677f
Sarah Wooders <sarahwooders@gmail.com>
2024-08-26 14:18:57 -0700

    fix: remove import

--- tests/test_agent_function_update.py
+++ tests/test_agent_function_update.py
@@ -4,12 +4,11 @@ import uuid
 
 import pytest
 
-from memgpt import constants, create_client
+from memgpt import create_client
 from memgpt.functions.functions import USER_FUNCTIONS_DIR
 from memgpt.schemas.message import Message
 from memgpt.settings import settings
 from memgpt.utils import assistant_function_to_tool, json_dumps, json_loads
-from tests.mock_factory.models import MockUserFactory
 from tests.utils import create_config, wipe_config
 
 
