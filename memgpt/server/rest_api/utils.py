619f990d48b4fc9ca176a051f491511a4a9765c3
cpacker <packercharles@gmail.com>
2024-08-28 10:05:47 -0700

    fix: patch stream_steps to properly trigger + return compact JSON instead of JSON with newlines

--- memgpt/server/rest_api/utils.py
+++ memgpt/server/rest_api/utils.py
@@ -1,12 +1,12 @@
+import json
 import traceback
 from enum import Enum
-from typing import AsyncGenerator, Generator, Union
+from typing import AsyncGenerator, Union
 
 from pydantic import BaseModel
 
 # from memgpt.orm.user import User
 # from memgpt.orm.utilities import get_db_session
-from memgpt.utils import json_dumps
 
 SSE_PREFIX = "data: "
 SSE_SUFFIX = "\n\n"
@@ -17,28 +17,8 @@ SSE_ARTIFICIAL_DELAY = 0.1
 def sse_formatter(data: Union[dict, str]) -> str:
     """Prefix with 'data: ', and always include double newlines"""
     assert type(data) in [dict, str], f"Expected type dict or str, got type {type(data)}"
-    data_str = json_dumps(data) if isinstance(data, dict) else data
+    data_str = json.dumps(data, separators=(",", ":")) if isinstance(data, dict) else data
     return f"data: {data_str}\n\n"
-
-
-async def sse_generator(generator: Generator[dict, None, None]) -> Generator[str, None, None]:
-    """Generator that returns 'data: dict' formatted items, e.g.:
-
-    data: {"id":"chatcmpl-9E0PdSZ2IBzAGlQ3SEWHJ5YwzucSP","object":"chat.completion.chunk","created":1713125205,"model":"gpt-4-0613","system_fingerprint":null,"choices":[{"index":0,"delta":{"tool_calls":[{"index":0,"function":{"arguments":"}"}}]},"logprobs":null,"finish_reason":null}]}
-
-    data: {"id":"chatcmpl-9E0PdSZ2IBzAGlQ3SEWHJ5YwzucSP","object":"chat.completion.chunk","created":1713125205,"model":"gpt-4-0613","system_fingerprint":null,"choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"tool_calls"}]}
-
-    data: [DONE]
-
-    """
-    try:
-        for msg in generator:
-            yield sse_formatter(msg)
-            if SSE_ARTIFICIAL_DELAY:
-                await asyncio.sleep(SSE_ARTIFICIAL_DELAY)  # Sleep to prevent a tight loop, adjust time as needed
-    except Exception as e:
-        yield sse_formatter({"error": f"{str(e)}"})
-    yield sse_formatter(SSE_FINISH_MSG)  # Signal that the stream is complete
 
 
 async def sse_async_generator(generator: AsyncGenerator, finish_message=True):
