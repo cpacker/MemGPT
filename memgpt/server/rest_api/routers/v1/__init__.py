aaec8bcbe90c9cf2e7daff65b1e166bb113c6791
cpacker <packercharles@gmail.com>
2024-09-08 19:09:21 -0700

    feat: add agents.py (raw)

--- memgpt/server/rest_api/routers/v1/__init__.py
+++ memgpt/server/rest_api/routers/v1/__init__.py
@@ -1,4 +1,5 @@
-# from memgpt.server.rest_api.routers.v1.agents import router as agents_router
+from memgpt.server.rest_api.routers.v1.agents import router as agents_router
+
 # from memgpt.server.rest_api.routers.v1.blocks import router as blocks_router
 # from memgpt.server.rest_api.routers.v1.humans import router as humans_router
 # from memgpt.server.rest_api.routers.v1.large_language_models import router as llm_router
@@ -9,7 +10,7 @@ from memgpt.server.rest_api.routers.v1.tools import router as tools_router
 ROUTERS = [
     tools_router,
     sources_router,
-    # agents_router,
+    agents_router,
     # llm_router,
     # personas_router,
     # humans_router,
