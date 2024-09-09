5cbc097203d36a3b781ce688786dbb0af841b5b3
cpacker <packercharles@gmail.com>
2024-09-08 20:25:45 -0700

    feat: added + fixed llms.py

---
+++ memgpt/server/rest_api/routers/v1/llms.py
@@ -0,0 +1,29 @@
+from typing import TYPE_CHECKING, List
+
+from fastapi import APIRouter, Depends
+
+from memgpt.schemas.llm_config import LLMConfig
+from memgpt.server.rest_api.utils import get_memgpt_server
+
+if TYPE_CHECKING:
+    from memgpt.server.server import SyncServer
+
+router = APIRouter(prefix="/models", tags=["models", "llms"])
+
+
+@router.get("/", response_model=List[LLMConfig])
+def list_models(
+    server: "SyncServer" = Depends(get_memgpt_server),
+):
+    server.get_current_user()
+
+    # currently, the server only supports one model, however this may change in the future
+    llm_config = LLMConfig(
+        model=server.server_llm_config.model,
+        model_endpoint=server.server_llm_config.model_endpoint,
+        model_endpoint_type=server.server_llm_config.model_endpoint_type,
+        model_wrapper=server.server_llm_config.model_wrapper,
+        context_window=server.server_llm_config.context_window,
+    )
+
+    return [llm_config]
