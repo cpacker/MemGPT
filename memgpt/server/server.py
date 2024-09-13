212f75d6516412159602cda0a516f2f08171e83f
Charles Packer <packercharles@gmail.com>
2024-09-13 15:21:42 -0700

    feat: add support for `user_id` in header (#1755)

--- memgpt/server/server.py
+++ memgpt/server/server.py
@@ -1771,6 +1771,20 @@ class SyncServer(Server):
         memgpt_agent = self._get_or_load_agent(agent_id=agent_id)
         return memgpt_agent.retry_message()
 
+    def set_current_user(self, user_id: Optional[str]):
+        """Very hacky way to set the current user for the server, to be replaced once server becomes stateless
+
+        NOTE: clearly not thread-safe, only exists to provide basic user_id support for REST API for now
+        """
+
+        # Make sure the user_id actually exists
+        if user_id is not None:
+            user_obj = self.get_user(user_id)
+            if not user_obj:
+                raise ValueError(f"User with id {user_id} not found")
+
+        self._current_user = user_id
+
     # TODO(ethan) wire back to real method in future ORM PR
     def get_current_user(self) -> User:
         """Returns the currently authed user.
@@ -1778,6 +1792,15 @@ class SyncServer(Server):
         Since server is the core gateway this needs to pass through server as the
         first touchpoint.
         """
+
+        # Check if _current_user is set and if it's non-null:
+        if hasattr(self, "_current_user") and self._current_user is not None:
+            current_user = self.get_user(self._current_user)
+            if not current_user:
+                warnings.warn(f"Provided user '{self._current_user}' not found, using default user")
+            else:
+                return current_user
+
         # NOTE: same code as local client to get the default user
         config = MemGPTConfig.load()
         user_id = config.anon_clientid
