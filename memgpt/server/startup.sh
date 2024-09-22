bdf07d9f909c1a05a903798d3481a653803e581d
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2024-09-22 14:06:37 -0700

    add postgres uri echo

--- memgpt/server/startup.sh
+++ memgpt/server/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Starting MEMGPT server..."
+# TODO: remove this when finished debugging
+echo "POSTGRES_URI: $POSTGRES_URI"
 if [ "$MEMGPT_ENVIRONMENT" = "DEVELOPMENT" ] ; then
     echo "Starting in development mode!"
     uvicorn memgpt.server.rest_api.app:app --reload --reload-dir /memgpt --host 0.0.0.0 --port 8083
