ce32a4719aa3f192d5312f62d1369519ba684ad8
Shubham Naik <shub@memgpt.ai>
2024-10-02 16:16:05 -0700

    chore: add health check route

--- letta/server/rest_api/routers/v1/health.py
+++ letta/server/rest_api/routers/v1/health.py
@@ -1,16 +1,15 @@
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING
 
-from fastapi import APIRouter, Body, Depends, HTTPException, Query
+from fastapi import APIRouter
 
 from letta.cli.cli import version
 from letta.schemas.health import Health
-from letta.server.rest_api.utils import get_letta_server
-from letta.server.server import SyncServer
 
 if TYPE_CHECKING:
     pass
 
 router = APIRouter(prefix="/health", tags=["health"])
+
 
 # Health check
 @router.get("/", response_model=Health, operation_id="health_check")
