4f7658e138e3f06b75a280b3b85b9f4fd8d315e2
Shubham Naik <shub@memgpt.ai>
2024-10-02 16:13:23 -0700

    feat: add health check route

--- letta/server/rest_api/routers/v1/__init__.py
+++ letta/server/rest_api/routers/v1/__init__.py
@@ -4,6 +4,7 @@ from letta.server.rest_api.routers.v1.jobs import router as jobs_router
 from letta.server.rest_api.routers.v1.llms import router as llm_router
 from letta.server.rest_api.routers.v1.sources import router as sources_router
 from letta.server.rest_api.routers.v1.tools import router as tools_router
+from letta.server.rest_api.routers.v1.health import router as health_router
 
 ROUTERS = [
     tools_router,
@@ -12,4 +13,5 @@ ROUTERS = [
     llm_router,
     blocks_router,
     jobs_router,
+    health_router,
 ]
