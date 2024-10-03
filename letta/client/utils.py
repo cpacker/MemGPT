80a8109b776b97d29cbca68f639588a126fbdc12
Yuxiang Wang <yw5aj@virginia.edu>
2024-10-02 23:06:13 -0700

    Fixed utils.py

--- letta/client/utils.py
+++ letta/client/utils.py
@@ -38,23 +38,20 @@ def pprint(messages):
 
     html_content = css_styles + "<div class='terminal'>"
     for message in messages:
-        date_str = message["date"]
-        date_formatted = datetime.fromisoformat(date_str.replace("Z", "+00:00")).strftime("%Y-%m-%d %H:%M:%S")
+        date_formatted = message.date.strftime("%Y-%m-%d %H:%M:%S")
 
-        if "function_return" in message:
-            return_string = message["function_return"]
-            return_status = message["status"]
-            html_content += f"<p><strong>🛠️ [{date_formatted}] Function Return ({return_status}):</strong></p>"
-            html_content += f"<p class='function-return'>{return_string}</p>"
-        elif "internal_monologue" in message:
+        if hasattr(message, 'function_return'):
+            html_content += f"<p><strong>🛠️ [{date_formatted}] Function Return ({message.status}):</strong></p>"
+            html_content += f"<p class='function-return'>{message.function_return}</p>"
+        elif hasattr(message, 'internal_monologue'):
             html_content += f"<p><strong>💭 [{date_formatted}] Internal Monologue:</strong></p>"
-            html_content += f"<p class='internal-monologue'>{message['internal_monologue']}</p>"
-        elif "function_call" in message:
-            html_content += f"<p><strong>🛠️ [[{date_formatted}] Function Call:</strong></p>"
-            html_content += f"<p class='function-call'>{message['function_call']}</p>"
-        elif "assistant_message" in message:
+            html_content += f"<p class='internal-monologue'>{message.internal_monologue}</p>"
+        elif hasattr(message, 'function_call'):
+            html_content += f"<p><strong>🛠️ [{date_formatted}] Function Call:</strong></p>"
+            html_content += f"<p class='function-call'>{message.function_call}</p>"
+        elif hasattr(message, 'assistant_message'):
             html_content += f"<p><strong>🤖 [{date_formatted}] Assistant Message:</strong></p>"
-            html_content += f"<p class='assistant-message'>{message['assistant_message']}</p>"
+            html_content += f"<p class='assistant-message'>{message.assistant_message}</p>"
         html_content += "<br>"
     html_content += "</div>"
 
