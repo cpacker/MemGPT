a6a7a908b2beddd43234309c63f8089940e44283
Kevin Lin <kevinlin@Kevins-MacBook-Pro.local>
2024-10-08 15:05:50 -0700

    pass in llm and embedding configs

--- tests/test_o1_agent.py
+++ tests/test_o1_agent.py
@@ -1,6 +1,8 @@
 from letta.client.client import create_client
 from letta.o1_agent import send_final_message, send_thinking_message
 from letta.schemas.agent import AgentType
+from letta.schemas.embedding_config import EmbeddingConfig
+from letta.schemas.llm_config import LLMConfig
 
 
 def test_o1_agent():
@@ -10,7 +12,12 @@ def test_o1_agent():
     thinking_tool = client.create_tool(send_thinking_message)
     final_tool = client.create_tool(send_final_message)
 
-    agent_state = client.create_agent(agent_type=AgentType.o1_agent, tools=[thinking_tool.name, final_tool.name])
+    agent_state = client.create_agent(
+        agent_type=AgentType.o1_agent,
+        tools=[thinking_tool.name, final_tool.name],
+        llm_config=LLMConfig.default_config("gpt-4"),
+        embedding_config=EmbeddingConfig.default_config("text-embedding-ada-002"),
+    )
     agent = client.get_agent(agent_id=agent_state.id)
     assert agent is not None
 
