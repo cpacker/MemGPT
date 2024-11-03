bb138a8757f887269b8708a3fb478989e79e27e5
Matt Zhou <mattzh1314@gmail.com>
2024-11-03 11:19:38 -0800

    remove dev artifacts

--- letta/services/organization_manager.py
+++ letta/services/organization_manager.py
@@ -56,9 +56,6 @@ class OrganizationManager:
                 org = OrganizationModel(name=self.DEFAULT_ORG_NAME, id=self.DEFAULT_ORG_ID)
                 org.create(session)
 
-            print("AAAA", org)
-            print("BBBB", org.created_at)
-
             return org.to_pydantic()
 
     @enforce_types
