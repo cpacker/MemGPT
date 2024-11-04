13dee7ed3b3574862f2ea94fdcff41c37ec1daf5
Matt Zhou <mattzh1314@gmail.com>
2024-11-04 10:51:18 -0800

    try again

--- alembic/versions/eff245f340f9_rename_block_name_to_block_template_name.py
+++ alembic/versions/eff245f340f9_rename_block_name_to_block_template_name.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "eff245f340f9"
-down_revision: Union[str, None] = "0c315956709d"
+down_revision: Union[str, None] = "ee50a967e090"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
