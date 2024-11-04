3b2790ce8497482de6cb41d032a18e63da11f8f2
Matt Zhou <mattzh1314@gmail.com>
2024-11-04 11:39:00 -0800

    import

--- letta/schemas/tool.py
+++ letta/schemas/tool.py
@@ -8,7 +8,10 @@ from letta.functions.helpers import (
     generate_crewai_tool_wrapper,
     generate_langchain_tool_wrapper,
 )
-from letta.functions.schema_generator import generate_schema_from_args_schema_v1
+from letta.functions.schema_generator import (
+    generate_schema_from_args_schema_v1,
+    generate_schema_from_args_schema_v2,
+)
 from letta.schemas.letta_base import LettaBase
 from letta.schemas.openai.chat_completions import ToolCall
 
