1b0b4add9b21cce5b4ae0da1bdb0de9641ddc584
Sarah Wooders <sarahwooders@gmail.com>
2024-11-07 08:47:48 -0800

    update startup.sh

--- letta/server/startup.sh
+++ letta/server/startup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-echo "Starting MEMGPT server..."
+echo "Starting Letta server at http://localhost:8283"
 
-alembic upgrade head
+# Check if LETTA_PG_URI is set and run alembic upgrade if it is
+if [ -n "$LETTA_PG_URI" ]; then
+    echo "LETTA_PG_URI is set, running alembic upgrade head"
+    alembic upgrade head
+fi
 
-if [ "$MEMGPT_ENVIRONMENT" = "DEVELOPMENT" ] ; then
+if [ "$MEMGPT_ENVIRONMENT" = "DEVELOPMENT" ]; then
     echo "Starting in development mode!"
     uvicorn letta.server.rest_api.app:app --reload --reload-dir /letta --host 0.0.0.0 --port 8283
 else
