7756f3bd9485c7bd7d01ccff1878aef0cf687fa0
Kevin Lin <kl2806@columbia.edu>
2024-11-14 16:00:16 -0800

    fix loop to exit

--- letta/offline_memory_agent.py
+++ letta/offline_memory_agent.py
@@ -1,10 +1,12 @@
+import json
 from typing import List, Optional, Union
 
-from letta.agent import Agent
+from letta.agent import Agent, save_agent
 from letta.interface import AgentInterface
 from letta.metadata import MetadataStore
 from letta.schemas.agent import AgentState
 from letta.schemas.message import Message
+from letta.schemas.openai.chat_completion_response import UsageStatistics
 from letta.schemas.tool import Tool
 from letta.schemas.usage import LettaUsageStatistics
 
@@ -42,14 +44,17 @@ def trigger_rethink_memory(self: "Agent", message: Optional[str]) -> Optional[st
             client.user_message(agent_id=agent.id, message=message)
 
 
-def rethink_memory(self: "Agent") -> Optional[str]:
+def rethink_memory(self, new_memory: str, block_label: Optional[str]) -> Optional[str]:
     """
-    Goes through the memory and rethinks the memory based on the new message.
+    Rethinks the memory in the memory block titled `block_name`,
+    and integrates the information from the memory block into the memory.
 
+    Args:
+        new_memory (str): The new memory with information integrated from the memory block.
+        block_label (str): The name of the block to integrate information from. None all the information has been integrated to terminate the loop.
     """
-    for memory_block in self.memory:
-        print(memory_block.value)
-    self.memory.update_block_value(name="rethink_memory_block", value="Rethinking memory")
+
+    self.memory.update_block_value(label="rethink_memory_block", value=new_memory)
     return None
 
 
@@ -73,5 +78,34 @@ class OfflineMemoryAgent(Agent):
         ms: Optional[MetadataStore] = None,
         **kwargs,
     ) -> LettaUsageStatistics:
-        """Go through what is currently in memory core memory and information."""
-        return super().step(messages, chaining, max_chaining_steps, ms, **kwargs)
+        """Go through what is currently in memory core memory and integrate information."""
+
+        next_input_message = messages if isinstance(messages, list) else [messages]
+        counter = 0
+        total_usage = UsageStatistics()
+        step_count = 0
+
+        current_block_label = "rethink_memory_block"
+        while current_block_label in self.memory.list_block_labels():
+            kwargs["ms"] = ms
+            kwargs["first_message"] = False
+            step_response = self.inner_step(
+                messages=next_input_message,
+                **kwargs,
+            )
+            print(step_response)
+            for message in step_response.messages:
+                if message.tool_calls:
+                    for tool_call in message.tool_calls:
+                        arguments = json.loads(tool_call.function.arguments)
+                        current_block_label = arguments["block_label"]
+
+            usage = step_response.usage
+            step_count += 1
+            total_usage += usage
+            counter += 1
+            self.interface.step_complete()
+            if ms:
+                save_agent(self, ms)
+
+        return LettaUsageStatistics(**total_usage.model_dump(), step_count=step_count)
--- tests/test_offline_memory_agent.py
+++ tests/test_offline_memory_agent.py
@@ -5,6 +5,7 @@ from letta.offline_memory_agent import (
     send_message_offline_agent,
     trigger_rethink_memory,
 )
+from letta.prompts import gpt_system
 from letta.schemas.agent import AgentType
 from letta.schemas.embedding_config import EmbeddingConfig
 from letta.schemas.llm_config import LLMConfig
@@ -20,11 +21,24 @@ def test_offline_memory_agent():
     send_message_offline_agent_tool = client.create_tool(send_message_offline_agent)
 
     conversation_memory = ChatMemory(human=get_human_text(DEFAULT_HUMAN), persona=get_persona_text(DEFAULT_PERSONA))
+    offline_memory = ChatMemory(human=get_human_text(DEFAULT_HUMAN), persona=get_persona_text("offline_memory_persona"))
+
+    previous_memories = [
+        Block(name="interaction_1", label="interaction_1", value="User clicked on product 2, and not product 1", limit=2000),
+        Block(name="interaction_2", label="interaction_2", value="User clicked on product 2 and not product 3", limit=2000),
+    ]
+
+    for memory in previous_memories:
+        conversation_memory.link_block(memory)
+        offline_memory.link_block(memory)
+
     new_memory = Block(name="rethink_memory_block", label="rethink_memory_block", value="", limit=2000)
     conversation_memory.link_block(new_memory)
+    offline_memory.link_block(new_memory)
 
     conversation_agent = client.create_agent(
-        agent_type=AgentType.offline_memory_agent,
+        agent_type=AgentType.memgpt_agent,
+        system=gpt_system.get_system_text("memgpt_convo_only"),
         llm_config=LLMConfig.default_config("gpt-4"),
         embedding_config=EmbeddingConfig.default_config("text-embedding-ada-002"),
         tools=[send_message_offline_agent_tool.name, trigger_rethink_memory_tool.name],
@@ -32,17 +46,22 @@ def test_offline_memory_agent():
         include_base_tools=False,
     )
     assert conversation_agent is not None
-    assert conversation_agent.memory.list_block_labels() == ["persona", "human", "rethink_memory_block"]
+    assert conversation_agent.memory.list_block_labels() == ["persona", "human", "interaction_1", "interaction_2", "rethink_memory_block"]
 
     rethink_memory_tool = client.create_tool(rethink_memory)
     offline_memory_agent = client.create_agent(
         agent_type=AgentType.offline_memory_agent,
-        memory=ChatMemory(human=get_human_text(DEFAULT_HUMAN), persona=get_persona_text("offline_memory_persona")),
+        system=gpt_system.get_system_text("memgpt_offline_memory"),
+        memory=offline_memory,
         llm_config=LLMConfig.default_config("gpt-4"),
         embedding_config=EmbeddingConfig.default_config("text-embedding-ada-002"),
         tools=[rethink_memory_tool.name],
+        include_base_tools=False,
     )
-    response = client.user_message(agent_id=conversation_agent.id, message="rethink: Tell me something I don't know about myself.")
+    assert offline_memory_agent is not None
+    assert offline_memory_agent.memory.list_block_labels() == ["persona", "human", "interaction_1", "interaction_2", "rethink_memory_block"]
+
+    response = client.user_message(agent_id=conversation_agent.id, message="rethink_memory")
     print(response)
 
 
