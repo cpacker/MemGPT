ac83f1e4ac24eab64a680abb2c073b4c48d9ce21
Sarah Wooders <sarahwooders@gmail.com>
2024-11-21 17:22:30 -0800

    fix tests

--- letta/schemas/tool.py
+++ letta/schemas/tool.py
@@ -6,10 +6,7 @@ from letta.functions.helpers import (
     generate_composio_tool_wrapper,
     generate_langchain_tool_wrapper,
 )
-from letta.functions.schema_generator import (
-    generate_schema_from_args_schema_v1,
-    generate_schema_from_args_schema_v2,
-)
+from letta.functions.schema_generator import generate_schema_from_args_schema_v2
 from letta.schemas.letta_base import LettaBase
 from letta.schemas.openai.chat_completions import ToolCall
 
@@ -131,7 +128,7 @@ class ToolCreate(LettaBase):
         tags = ["langchain"]
         # NOTE: langchain tools may come from different packages
         wrapper_func_name, wrapper_function_str = generate_langchain_tool_wrapper(langchain_tool, additional_imports_module_attr_map)
-        json_schema = generate_schema_from_args_schema_v1(langchain_tool.args_schema, name=wrapper_func_name, description=description)
+        json_schema = generate_schema_from_args_schema_v2(langchain_tool.args_schema, name=wrapper_func_name, description=description)
 
         return cls(
             name=wrapper_func_name,
