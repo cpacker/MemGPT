3682280e38a3e9f29fee35d3ec482ecd8bc9c723
Matt Zhou <mattzh1314@gmail.com>
2024-11-22 11:57:55 -0800

    Factor out constant template id

--- letta/settings.py
+++ letta/settings.py
@@ -12,7 +12,7 @@ class ToolSettings(BaseSettings):
 
     # Sandbox configurations
     e2b_api_key: Optional[str] = None
-    e2b_sandbox_template_id: Optional[str] = "m2lu1nfx7ztyiuzlbl89"  # Updated manually
+    e2b_sandbox_template_id: Optional[str] = None  # Updated manually
 
 
 class ModelSettings(BaseSettings):
