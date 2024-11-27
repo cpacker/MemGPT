cd492fa741ffe6cf3dc876f687302cf82a823413
cpacker <packercharles@gmail.com>
2024-11-26 16:47:12 -0800

    fix: added Dict to the list of typing globals available to the parser

--- letta/functions/functions.py
+++ letta/functions/functions.py
@@ -3,7 +3,7 @@ import inspect
 import os
 from textwrap import dedent  # remove indentation
 from types import ModuleType
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from letta.constants import CLI_WARNING_PREFIX
 from letta.errors import LettaToolCreateError
@@ -14,7 +14,7 @@ def derive_openai_json_schema(source_code: str, name: Optional[str] = None) -> d
     # auto-generate openai schema
     try:
         # Define a custom environment with necessary imports
-        env = {"Optional": Optional, "List": List}  # Add any other required imports here
+        env = {"Optional": Optional, "List": List, "Dict": Dict}  # Add any other required imports here
 
         env.update(globals())
         exec(source_code, env)
@@ -27,7 +27,7 @@ def derive_openai_json_schema(source_code: str, name: Optional[str] = None) -> d
         json_schema = generate_schema(func, name=name)
         return json_schema
     except Exception as e:
-        raise LettaToolCreateError(f"Failed to execute source code: {e}")
+        raise LettaToolCreateError(f"Failed to derive JSON schema from source code: {e}")
 
 
 def parse_source_code(func) -> str:
