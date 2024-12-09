3c4d7b7c35034004a55637b04a4fe3604956d2dc
Kevin Lin <kl2806@columbia.edu>
2024-12-09 11:16:59 -0800

    organize into dir

---
+++ fact_edit_experiments/README.md
@@ -0,0 +1,35 @@
+We evaluate on multi-hop memory edits, to see how well agents are able to assess and resolve inconsistencies in their memory.
+
+
+Get the data from the [MQuAKE: Assessing Knowledge Editing in Language Models via Multi-Hop Questions](https://arxiv.org/abs/2305.14795)
+```
+git clone https://github.com/princeton-nlp/MQuAKE.git
+```
+
+We use the latest recommended dataset version, which is `MQuAKE/datasets/MQuAKE-CF-3k-v2.json`. 
+
+Then, convert this dataset into our data format, for debugging purposes, also pass in `--num-questions` to run on a smaller set of instances.
+```
+python convert_mquake_to_explicit_format.py \
+    --input_file MQuAKE/datasets/MQuAKE-CF-3k-v2.json \
+    --output_file MQuAKE/datasets/letta-MQuAKE-CF-3k-v2-100.json \
+```
+
+
+The output of the previous generated step can then be fed to agents in the Letta framework.
+
+To generate predictions with just the baseline Letta agent, run:
+
+```
+python fact_edit_baseline.py \
+    --input_file_name MQuAKE/datasets/letta-MQuAKE-CF-3k-v2-100.json \
+    --predictions_file_name MQuAKE/datasets/predictions-letta-MQuAKE-CF-3k-v2-100-fact-prompt-v2-offline-memory.json
+```
+
+To run the async memory agent:
+```
+python fact_edit_baseline.py \
+    --input_file_name MQuAKE/datasets/letta-MQuAKE-CF-3k-v2-100.json \
+    --predictions_file_name MQuAKE/datasets/predictions-letta-MQuAKE-CF-3k-v2-100-fact-prompt-v2-offline-memory.json  \
+    --offline_memory
+```
--- fact_edit_experiments/convert_mquake_to_explicit_format.py
+++ fact_edit_experiments/convert_mquake_to_explicit_format.py
@@ -1,6 +1,9 @@
 """
 Script to convert MQuAKE data to letta form.
 
+Example:
+    
+    python convert_mquake_to_explicit_format.py --input_file MQuaAKE/datasets/MQuAKE-CF-3k-v2.json --output_file MQuAKE/datasets/letta-MQuAKE-CF-3k-v2.json --num_questions 1000
 
 """
 import argparse
@@ -8,20 +11,22 @@ from collections import defaultdict
 from openai import OpenAI
 import jsonlines
 import json
+from typing import Optional
 
 import tqdm
 
 def question_to_statement(question: str) -> str:
     """
-    Given a question, return a cloze statement with a blank for the answer.
+    Given a question, return a cloze statement with a blank for the answer. 
+    We use this to convert the questions into a chat message to the model
     """
     completion = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {
                 "role": "user",
-                "content": f"""Turn this question into a factual cloze statement with {{}} for where the answer should go, do not actually include the answer in the braces: \n\nQuestion: {question}\n\n Respond with just the statement only."""
+                "content": f"""Turn this question into a factual cloze statement with {{}} for where the answer should go, do not actually include the answer in the braces or any spaces between the braces. This is to be used for formatting.: \n\nQuestion: {question}\n\n Respond with just the statement only."""
             }
         ]
     )
@@ -29,14 +34,16 @@ def question_to_statement(question: str) -> str:
     
 client = OpenAI()
 
-def convert_explicit_statements(input_file_name: str, output_file_name: str, num_questions: int):
+def convert_explicit_statements(input_file_name: str, output_file_name: str, num_questions: Optional[int]):
     """
     Convert the MQuAKE data to the Letta format.
     """
     with open(input_file_name) as input_file:
         with jsonlines.open(output_file_name, 'w') as output_file:
             data = json.load(input_file)
-            for idx, datum in tqdm.tqdm(enumerate(data[:num_questions])):
+            if num_questions is None:
+                data = data[:num_questions]
+            for idx, datum in tqdm.tqdm(enumerate(data)):
                 letta_data = defaultdict(list)
                 for hop in datum["single_hops"]:
                     fact = f"{hop["cloze"]} {hop["answer"]}."
@@ -80,6 +87,6 @@ if __name__ == "__main__":
     argparse = argparse.ArgumentParser()
     argparse.add_argument("--input_file", type=str, required=True, default="MQuaAKE/datasets/MQuAKE-CF-3k-v2.json")
     argparse.add_argument("--output_file", type=str, required=True, default="MQuAKE/datasets/letta-MQuAKE-CF-3k-v2.json")
-    argparse.add_argument("--num_questions", type=int, required=True, default=100)
+    argparse.add_argument("--num_questions", type=int, required=False, default=None)
     args = argparse.parse_args()
     convert_explicit_statements(args.input_file, args.output_file, args.num_questions)
